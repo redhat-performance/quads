c2b160b804fab8c8b56480b41848e0c29f1ea285
Will Foster <wfoster@redhat.com>
2016-07-14 18:57:51 -0400

    minor spacing, comment cleanup

--- racks-wiki.py
+++ racks-wiki.py
@@ -2,7 +2,7 @@
 # Update an existing wordpress page with generated markdown
 # Assumes you have a markdown file with content you want published
 # to an existing wordpress page.
-# requires: python-wordpress-xmlrpc
+# requires: python2-wordpress-xmlrpc or python3-wordpress-xmlrpc
 
 from wordpress_xmlrpc import *
 from wordpress_xmlrpc.methods.taxonomies import *
@@ -19,9 +19,13 @@ wp = Client(wp_url, wp_username, wp_password)
 # define pages variable
 page = WordPressPage()
 page.title = 'RDU Scale Lab Racks'
+
 # page id can be found by viewing via wp-admin dashboard in URL
 page.id = 4
+
 # set local content file to read handle info into a string
 f = open('update-racks-page.md', 'r')
 page.content = f.read()
+
+# post new content to the page
 wp.call(EditPost(page.id, page))
