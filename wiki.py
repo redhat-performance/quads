e2d407d8712e3184e991e74f1874921f803cfd19
Will Foster <wfoster@redhat.com>
2016-07-14 14:30:56 -0400

    working, however page.content read in as markdown needs fixing

--- wiki.py
+++ wiki.py
@@ -2,22 +2,26 @@
 # Update an existing wiki page with generated markdown
 # requires: python-wordpress-xmlrpc
 
-from wordpress_xmlrpc import Client, WordPressPage
-from wordpress_xmlrpc.methods.posts import NewPost, EditPost
+from wordpress_xmlrpc import *
+from wordpress_xmlrpc.methods.taxonomies import *
+from wordpress_xmlrpc.methods.posts import *
+from wordpress_xmlrpc.methods.users import *
+from wordpress_xmlrpc.methods import *
 
 #authentication
-wp_url = "http://wiki.example.com/xmlrpc.php"
+wp_url = "http://wiki.example/xmlrpc.php"
 wp_username = "admin"
 wp_password = "admin"
 wp = Client(wp_url, wp_username, wp_password)
 
 # define pages variable
-pages = client.call(posts.GetPosts({'post_type': 'page'}, results_class=WordPressPage))
 page = WordPressPage()
 page.title = 'Example Scale Data Post'
 # page id can be found by viewing via wp-admin dashboard in URL
-page.id = "9"
-
+page.id = 9
 # pull page from generated/converted markdown
+# FIXME: probably need a markdown or similiar lib here
+# as it doesn't like page.content with open('file', 'r')
 page.content = open('/tmp/update-page.md', 'r')
-wp.call(posts.EditPost(page.id, page))
+# post page update
+wp.call(EditPost(page.id, page))
