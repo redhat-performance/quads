326d0b9c56eb02bef4d325d7c05bcba9e76e172d
Kambiz Aghaiepour <kambiz@redhat.com>
2016-11-16 14:11:41 -0500

    WIP: add config options
    Details:
    add a yaml file with config options.  This is a
    first pass to make the entire quads package portable
    and usable outside of our own environment.  The various
    scripts will need to be updated to read in the yaml from
    the config source (for both shell and python elements)
    
    Also adding the csv-to-instack.py used to generate instackenv.json
    
    This is related to: https://github.com/redhat-performance/quads/issues/1
    
    Change-Id: I6c052fe46617f3fbbdae8c9f0b1480d477fc1c87

---
+++ bin/csv-to-instack.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# This code was shamelessly plucked out of another
+# repo.  Originally written by Joe Talerico <jtaleric at redhat dot com>
+
+import csv
+import json
+import sys
+import getopt
+from collections import defaultdict
+
+def main(argv):
+    inputfile = None
+
+    try:
+        opts, args = getopt.getopt(argv,"c",["csv="])
+    except :
+        print >> sys.stderr, "csv-to-instack.py --csv=<inputfile>"
+        sys.exit(2)
+
+    for opt, arg in opts:
+       if opt == '-h':
+           print >> sys.stderr ,'csv-to-instack.py --csv=<inputfile>'
+           sys.exit()
+       elif opt in ("-c", "--csv"):
+           inputfile = arg
+    if inputfile == None :
+        print >> sys.stderr, "Error : No input file passed"
+        print >> sys.stderr, "Usage:"
+        print >> sys.stderr, "csv-to-instack.py --csv=<inputfile>"
+        sys.exit(2)
+
+    print >> sys.stderr, "Opening %s" % inputfile
+    csvFile =  open(inputfile)
+    data = list(csv.reader(csvFile))
+
+    firstrow = True
+    jdata = defaultdict(list)
+    for value in data:
+        if firstrow :
+            firstrow = False
+            continue
+        jdata['nodes'].append({'pm_password' : value[3],
+        'pm_type' : value[4],
+        'mac' : [value[0]],
+        'cpu' : "2",
+        'memory' : "1024",
+        'disk' : "20",
+        'arch' : "x86_64",
+        'pm_user' : value[2],
+        'pm_addr' : value[1]})
+
+    print json.dumps(jdata,indent=4, sort_keys=True)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
