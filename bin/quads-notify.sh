8f28bb6e3b0b85d5f4cd7ab9e830c68cd5b581a5
Will Foster <wfoster@redhat.com>
2016-11-22 15:03:36 +0000

    Expand upon notifications for QUADS.
    * Add optional IRC notifications for when new systems
      are provisioned and available.
    * Add optional email notifications for when new systems
      are provisioned and available
    * Rename alert-expiration to quads-notify
    
    patchset #2: whitespace
    
    fixes https://github.com/redhat-performance/quads/issues/2
    
    Change-Id: Ib49b5d5d92081b7bbfc1f23179f7096acaf79f8e

--- bin/quads-notify.sh
+++ bin/quads-notify.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#
-#  Send notifications when hosts are expiring.
-#
+#  Send email and IRC notifications when hosts are assigned
+#  Send email notifications when hosts are expiring.
 #######
 
 if [ ! -e $(dirname $0)/load-config.sh ]; then
@@ -9,6 +8,7 @@ if [ ! -e $(dirname $0)/load-config.sh ]; then
     exit 1
 fi
 
+# load the ../conf/quads.yml values as associative array
 source $(dirname $0)/load-config.sh
 
 quads=${quads["install_dir"]}/bin/quads.py
@@ -18,6 +18,49 @@ days="1 3 5 7"
 
 # only worry about environments with active hosts
 env_list=$($quads --summary | awk '{ print $1 }')
+
+function craft_initial_message() {
+    msg_file=$(mktemp /tmp/msgXXXXXXXXXX)
+    owner=$1
+    env_to_report=$2
+    ircbot_ipaddr=${quads["ircbot_ipaddr"]}
+    ircbot_port=${quads["ircbot_port"]}
+    report_file=${env_to_report}-${owner}-initial-$($quads --ls-ticket --cloud-only ${env_to_report})
+    if [ ! -f ${data_dir}/report/${report_file} ]; then
+        touch ${data_dir}/report/${report_file}
+        if ${quads["email_notify"]} ; then
+            cat > $msg_file <<EOF
+To: $owner@${quads["domain"]}
+Cc: ${quads["report_cc"]}
+Subject: New QUADS Assignment Allocated
+From: QUADS <quads@${quads["domain"]}>
+Reply-To: dev-null@${quads["domain"]}
+
+Greetings Citizen,
+
+You've been allocated a new environment!
+
+   $env_to_report
+
+(Details)
+http://${quads["wp_wiki"]}/assignments/#$env_to_report
+
+You can view your machine list, duration and other
+details above.
+
+DevOps Team
+
+EOF
+            /usr/sbin/sendmail -t < $msg_file 1>/dev/null 2>&1
+        fi
+        if ${quads["irc_notify"]} ; then
+            # send IRC notification
+            echo "QUADS: $env_to_report is now active - http://${quads["wp_wiki"]}/assignments/#$env_to_report" | nc -w 1 $ircbot_ipaddr $ircbot_port
+
+        fi
+    fi
+    rm -f $msg_file
+}
 
 function craft_message() {
 
@@ -30,7 +73,8 @@ function craft_message() {
     report_file=${env_to_report}-${owner}-${days_to_report}-$($quads --ls-ticket --cloud-only ${env_to_report})
     if [ ! -f ${data_dir}/report/${report_file} ]; then
         touch ${data_dir}/report/${report_file}
-        cat > $msg_file <<EOF
+        if ${quads["email_notify"]} ; then
+            cat > $msg_file <<EOF
 To: $owner@${quads["domain"]}
 Cc: ${quads["report_cc"]}
 Subject: QUADS upcoming expiration notification
@@ -54,12 +98,14 @@ Thank you for your attention.
 DevOps Team
 
 EOF
-        /usr/sbin/sendmail -t < $msg_file 1>/dev/null 2>&1
+            /usr/sbin/sendmail -t < $msg_file 1>/dev/null 2>&1
+        fi
     fi
     rm -f $msg_file
 }
 
 for e in $env_list ; do
+    craft_initial_message $($quads --cloud-only $e --ls-owner) $e
     alerted=false
     for d in $days ; do
         # if "nobody" is the owner you can skip it...
