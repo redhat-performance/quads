f028f1d37d3184df2abdca2458877df3f410e306
Kambiz Aghaiepour <kambiz@redhat.com>
2017-02-02 12:45:46 -0500

    Add ansible playbooks for changing boot order.
    This patch also includes a fix for the notifications
    as an environment is prepped.  Before the "go-live" time
    is reached we need to look at the full summary of the
    defined environments.
    
    This patch also adds the username/pass for the foreman
    views to be included in the notifications.
    
    fixes: https://github.com/redhat-performance/quads/issues/18
    resolves: https://trello.com/c/YIJB9muP/10-quads-reconfiguration-of-boot-sequence-when-reservation-expires
    
    Change-Id: Iaf5055c153fb56a6fbff7e5fb19fea1377d8dc40

---
+++ bin/quads-validate-boot-order.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+if [ ! -e $(dirname $0)/load-config.sh ]; then
+    echo "$(basename $0): could not find load-config.sh"
+    exit 1
+fi
+
+source $(dirname $0)/load-config.sh
+
+sleep 10
+
+data_dir=${quads["data_dir"]}
+quads=${quads["install_dir"]}/bin/quads.py
+foreman_param=${quads["foreman_director_parameter"]}
+lockdir=$data_dir/lock
+
+[ ! -d $lockdir ] && mkdir -p $lockdir
+
+PIDFILE=$lockdir/quads-move.pid
+
+if [ -f $PIDFILE ]; then
+    if [ -d /proc/$(cat $PIDFILE) ]; then
+        echo Another instance already running. Try again later.
+        exit 1
+    fi
+fi
+
+echo $$ > $PIDFILE
+
+if [ ! -d $data_dir/boot ]; then
+    mkdir $data_dir/boot
+fi
+
+if [ ! -d $data_dir/bootstate ]; then
+    mkdir $data_dir/bootstate
+fi
+
+for h in $(hammer host list --search params.${foreman_param}=true | grep redhat.com  | awk '{ print $3 }' ) ; do
+    if [ -f $data_dir/bootstate/$h ]; then
+        current_state=$(cat $data_dir/bootstate/$h)
+        if [ "$current_state" != "director" ]; then
+            build_state=$(hammer host info --name $h | grep Build | awk '{ print $NF }')
+            if [ "$build_state" = "no" ]; then
+                echo director > $data_dir/boot/$h
+            fi
+        fi
+    fi
+done
+
+for h in $(hammer host list --search params.${foreman_param}=false | grep redhat.com  | awk '{ print $3 }' ) ; do
+    if [ -f $data_dir/bootstate/$h ]; then
+        current_state=$(cat $data_dir/bootstate/$h)
+        if [ "$current_state" != "foreman" ]; then
+            build_state=$(hammer host info --name $h | grep Build | awk '{ print $NF }')
+            if [ "$build_state" = "no" ]; then
+                echo foreman > $data_dir/boot/$h
+            fi
+        fi
+    fi
+done
+
