b8c3f979a8899d86b5a22d8576d531d4689f8b07
Will Foster <wfoster@redhat.com>
2017-03-04 17:50:49 +0000

    Defined cloud range loaded via libquads
    Currently simple-table-generator.py uses a range within
    the tool to define the in-use start/end clouds in order to
    generate heat map visualizations.  When you exceed this number
    of defined clouds it breaks the generation.
    
    This will load and query the QUADS object instead and obtain
    the range of defined clouds so we know what to generate the
    visualizations based on.
    
    fixes https://github.com/redhat-performance/quads/issues/37
    
    Change-Id: If74744bbdb9abcf0634b820b1f97f46423d710c2

--- bin/simple-table-generator.py
+++ bin/simple-table-generator.py
@@ -25,9 +25,70 @@ host_color_file = args.host_color_file
 days = args.days
 gentime = args.gentime
 
-def get_spaced_colors():
-    return [(190,193,212),(2,63,165),(216,19, 19),(187,119,132),(142,6,59),(74,111,227),(230,175,185),(211,63,106),(17,198,56),(239,151,8),(15,207,192),(247,156,212)]
+# Load QUADS yaml config
+def quads_load_config(quads_config):
+    try:
+        stream = open(quads_config, 'r')
+        try:
+            quads_config_yaml = yaml.load(stream)
+            stream.close()
+        except Exception, ex:
+            print "quads: Invalid YAML config: " + quads_config
+            exit(1)
+    except Exception, ex:
+        print ex
+        exit(1)
+    return(quads_config_yaml)
 
+quads_config_file = os.path.dirname(__file__) + "/../conf/quads.yml"
+quads_config = quads_load_config(quads_config_file)
+
+# Sanity checks to determine QUADS dir structure is intact
+if "data_dir" not in quads_config:
+    print "quads: Missing \"data_dir\" in " + quads_config_file
+    exit(1)
+
+if "install_dir" not in quads_config:
+    print "quads: Missing \"install_dir\" in " + quads_config_file
+    exit(1)
+
+sys.path.append(quads_config["install_dir"] + "/lib")
+sys.path.append(os.path.dirname(__file__) + "/../lib")
+
+# Load QUADS object
+import libquads
+
+defaultstatedir = quads_config["data_dir"] + "/state"
+defaultmovecommand = "/bin/echo"
+
+quads = libquads.Quads(quads_config["data_dir"] + "/schedule.yaml",
+                       defaultstatedir, defaultmovecommand,
+                       None, None, False, False)
+
+# Set maxcloud to maximum defined clouds
+maxcloud = len(quads.quads.clouds.data)
+
+# define color palette
+# https://www.google.com/#q=rgb+color+picker&*
+def get_spaced_colors():
+    return [(190,193,212),
+            (2,63,165),
+            (216,19, 19),
+            (187,119,132),
+            (142,6,59),
+            (74,111,227),
+            (230,175,185),
+            (211,63,106),
+            (17,198,56),
+            (239,151,8),
+            (15,207,192),
+            (247,156,212),
+            (229, 244, 66),
+            (107, 66, 6),
+            (161, 0, 255),
+            (155, 255, 140)]
+
+# covert palette to hex
 def get_cell_color(a, b, c):
     return "#" + ('0' if len(hex(a)) < 4 else '') + hex(a)[2:] + ('0' if len(hex(b)) < 4 else '') + hex(b)[2:] + ('0' if len(hex(c)) < 4 else '') + hex(c)[2:]
 
@@ -54,7 +115,8 @@ def print_simple_table(data, data_colors, days):
     print "<th>Name</th>"
     print "<th>Color</th>"
     print "</tr>"
-    for i in range(0, 10):
+    # Here we generate the range of defined clouds for visuals
+    for i in range(0, int(maxcloud)):
         print "<tr>"
         print "<td> cloud" + ('0' if i < 9 else '') + str(i+1) + " </td>"
         print "<td bgcolor=\"" + str(color_array[i]) + "\"></td>"
