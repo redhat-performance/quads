946ab0b8ce58c9402f63c325e29b37a054b2f500
Will Foster <wfoster@redhat.com>
2017-03-21 09:31:00 +0000

    Make ansible_max_proc: a variable.
    We recently implemented ansible-playbook process limitations
    in order to manage system load better in response to a large
    amount of provisioning actions occurring simultaneously:
    
    https://github.com/redhat-performance/quads/commit/951f62afdeaadffa747f053a234a476f9864c6bd
    
    This patch utilizes a variable for the maximum ansible process
    count so it's more configurable.
    
    ansible_max_proc: 60
    
    We're setting 60 to be a sensible default.
    
    I'm also updating the contributing part of the README.md
    so that first-time usage of Gerrithub is better documented.
    
    Change-Id: I17983aced4e5b6bdb5f48bf673459dc0b76243eb

--- bin/quads-boot-order.sh
+++ bin/quads-boot-order.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# This is only used for Dell servers that require toggling
+# the PXE flag and interface order on the provisioning
+# interface to accomodate # OpenStack Directory-deployed clouds.
+# Other server types may require something similiar if they have
+# more advanced out-of-band boot orderering and persistence options.
+# More simple server IPMI implementations (like Supermicro) do not
+# need this level of accomodation for OSP Director.
+# See:
+# https://github.com/redhat-performance/quads/tree/master/templates
+# https://github.com/redhat-performance/quads/tree/master/ansible
 
 if [ ! -e $(dirname $0)/load-config.sh ]; then
     echo "$(basename $0): could not find load-config.sh"
@@ -10,6 +20,7 @@ source $(dirname $0)/load-config.sh
 quads=${quads["install_dir"]}/bin/quads.py
 install_dir=${quads["install_dir"]}
 data_dir=${quads["data_dir"]}
+ansible_max_proc=${quads["ansible_max_proc"]}
 
 function reconfigure() {
     target=$1
@@ -55,7 +66,7 @@ function reconfigure() {
     fi
 
     # safety check to avoid too many processes running at once
-    if [ "$(pgrep -f ansible-playbook -c)" -gt 60 ]; then
+    if [ "$(pgrep -f ansible-playbook -c)" -gt $ansible_max_proc ]; then
         echo Too many ansible-playbook processes running.  Try again later.
         rm -f $host_inventory
         return
