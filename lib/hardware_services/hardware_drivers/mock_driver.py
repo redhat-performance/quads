a7a2f3cffc5df201a345c72bec074bb129b010c4
djfinn14 <djfinn14@gmail.com>
2017-03-24 19:29:10 -0400

    first attempt at mock driver

--- lib/hardware_services/hardware_drivers/mock_driver.py
+++ lib/hardware_services/hardware_drivers/mock_driver.py
@@ -1,1 +1,38 @@
 # inherits from hardware_service.py and overwrites methods. This is a mock driver for testing purposes and will not be attached to any specific hardware
+from datetime import datetime
+import calendar
+import time
+import yaml
+import argparse
+import os
+import sys
+import requests
+import logging
+from subprocess import call
+from subprocess import check_call
+
+from hardware_service import HardwareService
+
+class MockDriver(object):
+    
+    
+    def update_cloud(self, **kwargs):
+        print "updated cloud"
+
+    def update_host(self, **kwargs):
+        print "Updated host"
+
+    def remove_cloud(self, **kwargs):
+        print "removed cloud"
+
+    def remove_host(self, **kwargs):
+        print "removed host"
+
+    def move_hosts(self, **kwargs):
+        print "moved hosts"
+
+    def list_clouds(self):
+        print "list clouds"
+    
+    def list_hosts(self):
+        print "list hosts"
