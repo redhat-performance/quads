35c34d6fadeafe4a79d856e3144ba8b819995cb3
Kambiz Aghaiepour <kambiz@redhat.com>
2017-03-27 19:57:44 -0400

    Only regenerate the wiki content when it changes.
    Optionally also store the generate main and assigments
    pages in a git repo.  Setup of git repo is not done in
    quads and is left to the end user.
    
    fixes: https://github.com/redhat-performance/quads/issues/52
    
    Change-Id: I2c0a6d94d9f1288a80d67431cd0d327c71a2506d

--- bin/regenerate-wiki.sh
+++ bin/regenerate-wiki.sh
@@ -17,6 +17,8 @@ wp_wiki_main_title=${quads["wp_wiki_main_title"]}
 wp_wiki_main_page_id=${quads["wp_wiki_main_page_id"]}
 wp_wiki_assignments_title=${quads["wp_wiki_assignments_title"]}
 wp_wiki_assignments_page_id=${quads["wp_wiki_assignments_page_id"]}
+wp_wiki_git_manage=${quads["wp_wiki_git_manage"]}
+wp_wiki_git_repo_path=${quads["wp_wiki_git_repo_path"]}
 
 
 lockfile=$data_dir/.wiki_regenerate
@@ -41,7 +43,25 @@ fi
 if grep -q Traceback $tmpfile ; then
     exit 1
 fi
-$bindir/racks-wiki.py --markdown $tmpfile --wp-url http://$wp_wiki/xmlrpc.php --wp-username  $wp_username --wp-password  $wp_password --page-title "$wp_wiki_main_title" --page-id $wp_wiki_main_page_id
+if $wp_wiki_git_manage ; then
+    if [ ! -d $wp_wiki_git_repo_path ]; then
+        exit 1
+    fi
+    cp $tmpfile $wp_wiki_git_repo_path/main.md
+    pushd $wp_wiki_git_repo_path
+    git commit -a -m "$(date) content update"
+    export PAGER=cat
+    GITDIFF="$(git diff origin/master)"
+    if [ -z "$GITDIFF" ]; then
+        :
+    else
+        $bindir/racks-wiki.py --markdown $tmpfile --wp-url http://$wp_wiki/xmlrpc.php --wp-username  $wp_username --wp-password  $wp_password --page-title "$wp_wiki_main_title" --page-id $wp_wiki_main_page_id
+        git push
+    fi
+    popd
+else
+    $bindir/racks-wiki.py --markdown $tmpfile --wp-url http://$wp_wiki/xmlrpc.php --wp-username  $wp_username --wp-password  $wp_password --page-title "$wp_wiki_main_title" --page-id $wp_wiki_main_page_id
+fi
 $bindir/create-input-assignments.sh 1>$tmpfile  2>&1
 if [ $? -gt 0 ]; then
     exit 1
@@ -49,6 +69,24 @@ fi
 if grep -q Traceback $tmpfile ; then
     exit 1
 fi
-$bindir/racks-wiki.py --markdown $tmpfile --wp-url http://$wp_wiki/xmlrpc.php --wp-username  $wp_username --wp-password  $wp_password --page-title "$wp_wiki_assignments_title" --page-id $wp_wiki_assignments_page_id
+if $wp_wiki_git_manage ; then
+    if [ ! -d $wp_wiki_git_repo_path ]; then
+        exit 1
+    fi
+    cp $tmpfile $wp_wiki_git_repo_path/assignments.md
+    pushd $wp_wiki_git_repo_path
+    git commit -a -m "$(date) content update"
+    export PAGER=cat
+    GITDIFF="$(git diff origin/master)"
+    if [ -z "$GITDIFF" ]; then
+        :
+    else
+        $bindir/racks-wiki.py --markdown $tmpfile --wp-url http://$wp_wiki/xmlrpc.php --wp-username  $wp_username --wp-password  $wp_password --page-title "$wp_wiki_assignments_title" --page-id $wp_wiki_assignments_page_id
+        git push
+    fi
+    popd
+else
+    $bindir/racks-wiki.py --markdown $tmpfile --wp-url http://$wp_wiki/xmlrpc.php --wp-username  $wp_username --wp-password  $wp_password --page-title "$wp_wiki_assignments_title" --page-id $wp_wiki_assignments_page_id
+fi
 rm -f $tmpfile $lockfile
 
