93fd9af22c8b49f2d961021dce24b0987da7a628
sarthakjagetia <sarthakj@bu.edu>
2017-03-28 18:59:58 -0400

    IT WORKS

--- lib/hardware_services/hardware_drivers/hil_driver.py
+++ lib/hardware_services/hardware_drivers/hil_driver.py
@@ -1,2 +1,45 @@
 # this class will inherit from hardware_service.py and overwrite all of its methods
 # with hil-specific behaviors - mostly through api calls to the HIL server
+# this class will inherit from hardware_service.py and overwrite all of its methods
+# with hil-specific behaviors - mostly through api calls to the HIL server
+
+from datetime import datetime
+import calendar
+import time
+import yaml
+import argparse
+import os
+import sys
+import requests
+import logging
+from subprocess import call
+from subprocess import check_call
+
+from hardware_services.hardware_service import HardwareService
+
+# added for EC528 HIL-QUADS integration project - not a good place for this variable - should be moved eventually
+hil_url = 'http://127.0.0.1:5000'
+
+class HilDriver(HardwareService):
+    def update_cloud(self, quadsinstance, **kwargs):
+        print "updated cloud"
+
+    def update_host(self, quadsinstance, **kwargs):
+        print "Updated host"
+
+    def remove_cloud(self, quadsinstance, **kwargs):
+        print "removed cloud"
+
+    def remove_host(self, quadsinstance, **kwargs):
+        print "removed host"
+
+    def move_hosts(self, quadsinstance, **kwargs):
+        print "moved hosts"
+
+    def list_clouds(self, quadsinstance):
+        #print "list clouds"
+	quadsinstance.quads_rest_call("GET", hil_url, '/projects') 
+
+    def list_hosts(self, quadsinstance):
+        #print "list hosts"
+	quadsinstance.quads_rest_call("GET", hil_url, '/nodes/all') #Testing 
