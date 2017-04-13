749ac82c817ad753ec808dc8b2efb2cb548f04dc
djfinn14 <djfinn14@gmail.com>
2017-04-13 00:39:13 -0400

    PyTest for MockInventoryDriver for Demo4 This is the PyTest that can be used to test the MockInventory Driver for Demo4. Pushing up for better testing from others.

--- testing/test_inventory.py
+++ testing/test_inventory.py
@@ -13,86 +13,107 @@ import os
 import sys
 import subprocess as sp
 
-sys.path.append(os.path.dirname(__file__) + "/../lib")
-sys.path.append(os.path.dirname(__file__) + "/../lib/hardware_services/inventory_drivers/")
-sys.path.append(os.path.dirname(__file__) + "/../lib/hardware_services/")
-import libquads
+@pytest.fixture(scope='function', autouse=True)
+def quads_init():
+    args = Test_Inventory.quads + " --init --force"
+    sp.call(args, shell=True)
 
-@pytest.fixture(scope="function")
-def quads_setup():
-    libquads.Quads("","","","","","","","")
-   # args = Test_Inventory.quads + " --init --force"
-  #  sp.call(args, shell=True)
+    args = Test_Inventory.quads + " --define-cloud cloud01 --description cloud01"
+    sp.call(args, shell=True)
 
-@pytest.fixture
+    args = Test_Inventory.quads + " --define-cloud cloud02 --description cloud02"
+    sp.call(args, shell=True)
+
+    args = Test_Inventory.quads + " --define-host host01 --default-cloud cloud02"	
+    sp.call(args, shell=True) == 1	
+
+@pytest.fixture(scope='function')
 def quads_config():
     pass
 
-@pytest.mark.userfixed("quads_setup")
 class Test_Inventory:
     quads = "../bin/quads.py"
-    
+   
     def test_quads_init_fail(self):
-        pass
-        inventoryservice = "Mock"
-        libquads.Quads("","","","","","","",inventoryservice) == 1
-    #    args = Test_Inventory.quads + " --init"
-     #   assert sp.call(args, shell=True) == 1
+       
+        args = Test_Inventory.quads + " --init"
+        assert sp.call(args, shell=True) == 1
 
     def test_quads_init_pass(self):
-        pass
-      #  args = Test_Inventory.quads + " --init --force"
-       # assert sp.call(args, shell=True) == 0
-    
-    def test_update_clouds(self):
-        pass
-       # quads_init()
+       
+        args = Test_Inventory.quads + " --init --force"
+        assert sp.call(args, shell=True) == 0
+
+    def test_update_clouds_pass(self):
         
-       # args = Test_Inventory.quads + " --define-cloud cloud01 --description cloud01"
-       # assert sp.call(args, shell=True) == 0
+        args = Test_Inventory.quads + " --define-cloud cloud03 --description cloud03"
+        assert sp.call(args, shell=True) == 0
+   
+    def test_update_clouds_fail(self):
         
+        args = Test_Inventory.quads + " --define-cloud cloud01 --description cloud01"
+        assert sp.call(args, shell=True) == 1
+       
     def test_update_hosts_pass(self):
-        pass
-      #  quads_init()
-        
-      #  args = Test_Inventory.quads + " --define-cloud host01 --default-cloud cloud01"
-      #  assert sp.call(args, shell=True) == 0
-        
-        
+       
+        args = Test_Inventory.quads + " --define-host host05 --default-cloud cloud01"
+        assert sp.call(args, shell=True) == 0
+
+    def test_update_hosts_fail(self):
+       
+        args = Test_Inventory.quads + " --define-host host01 --default-cloud cloud02"	
+        assert sp.call(args, shell=True) == 1      
+       
+    def test_remove_host_pass(self):
+       
+        args = Test_Inventory.quads + " --rm-host host01"
+        assert sp.call(args, shell=True) == 0
+
+    def test_remove_host_fail(self):
+       
+        args = Test_Inventory.quads + " --rm-host host10"
+        assert sp.call(args, shell=True) == 1
+       
+    def test_remove_cloud_pass(self):
+       
+        args = Test_Inventory.quads + " --rm-cloud cloud01"
+        assert sp.call(args, shell=True) == 0
+
+    def test_remove_cloud_fail(self):
+       
+        args = Test_Inventory.quads + " --rm-cloud cloud10"
+        assert sp.check_output(args, shell=True) == 'cloud10 not found\n'
+
+    def test_list_hosts_pass(self):
+
+	args = Test_Inventory.quads + " --define-host host01 --default-cloud cloud01"
+        sp.call(args, shell=True)
+       
+        args = Test_Inventory.quads + " --ls-hosts"
+        assert sp.check_output(args, shell=True) == "host01\n"
+       
+    def test_list_hosts_fail(self):
+       
+        args = Test_Inventory.quads + " --ls-hosts"
+	with pytest.raises(AssertionError):
+            assert sp.check_output(args, shell=True) == "listing"
+       
+    def test_list_clouds_pass(self):
+       
+        args = Test_Inventory.quads + " --ls-clouds"
+        assert sp.check_output(args, shell=True) == "cloud01\ncloud02\n"
+
+    def test_list_clouds_fail(self):
+       
+        args = Test_Inventory.quads + " --ls-clouds"
+	with pytest.raises(AssertionError):
+            assert sp.check_output(args, shell=True) == "cloud02\n"
+
+           
     def test_write_data(self):
-        
+       
         pass
-        
+       
     def test_sync_state(self):
-        
+       
         pass
-        
-        
-    def test_remove_host(self):
-        pass
-      #  quads_init()
-        
-      #  args = Test_Inventory.quads + " --rm-host host01"
-      #  sp.call(args, shell=True)
-        
-    def test_remove_cloud(self):
-        pass
-      #  quads_init()
-        
-      #  args = Test_Inventory.quads + " --rm-cloud cloud01"
-      #  sp.call(args, shell=True)
-        
-    def test_list_hosts(self):
-        pass
-      #  quads_init()
-        
-      #  args = Test_Inventory.quads + " --ls-hosts"
-      #  sp.call(args, shell=True)
-        
-    def test_list_clouds(self):
-        pass
-      #  quads_init()
-        
-      #  args = Test_Inventory.quads + " --ls-clouds"
-      #  sp.call(args, shell=True)
-    
