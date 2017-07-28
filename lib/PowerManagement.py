97c2febaf5bec7144afc8c0778b31a24bbed96f2
Joe Talerico <jtaleric@redhat.com>
2017-07-28 16:12:02 -0400

    WIP Power Management
    Currently a WIP. Need to get the ssh login/query peice sorted out. I can
    currenly query all the outlets, using SSH. Need to have these mapped out
    to specific hosts.
    
    Outlets - New data structure to be used across vendor poewr strips.
    
    Also, in the Host data structure, we will need to add the type of Power
    Management, and the location -- this will have to be sorted out.
    
    We might want to query the amount of power each strip is taking, we can
    query this, however, to attempt to keep it generic, we might want to
    create a "Strip" data struct where we store it. This could be easily
    contained in a dict,however, I think it would be easier for other
    implementations to create a object for bookkeeping.
    
    Change-Id: I5a54bd4d43643d09d3f593fc2f58d77086fa2534

---
+++ lib/PowerManagement.py
@@ -0,0 +1,36 @@
+# This file is part of QUADs.
+#
+# QUADs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QUADs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QUADs.  If not, see <http://www.gnu.org/licenses/>.
+
+import abc
+
+class PowerManagement(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def on(self):
+        pass
+
+    @abc.abstractmethod
+    def off(self):
+        pass
+
+    @abc.abstractmethod
+    def reset(self):
+        pass
+
+    @abc.abstractmethod
+    def drain(self):
+        pass
