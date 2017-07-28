874bba1a2b60084488b2ab6c8e9398a248f7a94a
Joe Talerico <jtaleric@redhat.com>
2017-07-28 16:12:02 -0400

    WIP Power Management
    Currently a WIP. Need to get the ssh login/query peice sorted out. I can
    currenly query all the outlets, using SSH. Need to have these mapped out
    to specific hosts.
    
    Outlets - New data structure to be used across vendor poewr strips.
    
    Also, in the Host data structure, we will need to add the type of Power
    Management, and the location -- this will have to be sorted out.
    
    We might want to query the amount of power each strip is taking, we can
    query this, however, to attempt to keep it generic, we might want to
    create a "Strip" data struct where we store it. This could be easily
    contained in a dict,however, I think it would be easier for other
    implementations to create a object for bookkeeping.
    
    Change-Id: I5a54bd4d43643d09d3f593fc2f58d77086fa2534

---
+++ lib/TestConnect.py
@@ -0,0 +1,40 @@
+import ServerTech
+import pprint
+import time
+import logging
+import sys
+
+def main() :
+    logger = logging.getLogger('quads')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.info("PowerStrip Work")
+
+    # Old ass SSH crap
+    options = {
+            "KexAlgorithms": "+diffie-hellman-group1-sha1",
+            "HostKeyAlgorithms": "+ssh-dss",
+            "StrictHostKeyChecking": "no",
+            "UserKnownHostsFile": "/dev/null"}
+
+    s = ServerTech.ServerTech(user="admn",
+                          password="admn",
+                          host="",
+                          options=options)
+
+    logger.info("Current state of BC7 : {}".format(s.state(".BC7")))
+    logger.info("Turning off BC7")
+    s.off(".BC7")
+    logger.info("Current state of BC7 : {}".format(s.state(".BC7")))
+    logger.info("Turning on BC7")
+    s.on(".BC7")
+    logger.info("Current state of BC7 : {}".format(s.state(".BC7")))
+    s.powerstrip_usage()
+    s.get_powerstrip_usage()
+
+if __name__ == '__main__':
+    sys.exit(main())
