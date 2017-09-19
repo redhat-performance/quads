8e531878b448c35dfa516124195859afb3f31d90
Kambiz Aghaiepour <kambiz@redhat.com>
2017-09-19 16:16:59 -0400

    Simplify move and switchconf verify scripts
    When the VLAN allocations per cloud environment
    are configured to be sequential and have a formula
    that can be applied, we no longer need to have
    the associative arrays that would need to be updated
    with each expansion of upper limits to supported
    cloud numbers.
    
    e.g. we use the following formula:
    
    Each cloud has 4 vlans.  The clouds are defined as: "cloudXX"
    XX is a number (e.g. your environment might support 01 through 24).
    
      offset = 1090 + (XX * 10)
      first_vlan = $offset
      last_vlan = $offset + 3
    
    e.g. cloud01 would be 1100 through 1103, cloud02 would be 1110 through
    1113, etc.
    
    Change-Id: I4953d408adb4a8b37c86990eb8eb2674950439c4

--- bin/move-and-rebuild-host.sh
+++ bin/move-and-rebuild-host.sh
@@ -63,22 +63,7 @@ fi
 
 expect_script=$bindir/juniper-set-port.exp
 
-declare -A cloud01=( ["em1"]="1100" ["em2"]="1101" ["em3"]="1102" ["em4"]="1103")
-declare -A cloud02=( ["em1"]="1110" ["em2"]="1111" ["em3"]="1112" ["em4"]="1113")
-declare -A cloud03=( ["em1"]="1120" ["em2"]="1121" ["em3"]="1122" ["em4"]="1123")
-declare -A cloud04=( ["em1"]="1130" ["em2"]="1131" ["em3"]="1132" ["em4"]="1133")
-declare -A cloud05=( ["em1"]="1140" ["em2"]="1141" ["em3"]="1142" ["em4"]="1143")
-declare -A cloud06=( ["em1"]="1150" ["em2"]="1151" ["em3"]="1152" ["em4"]="1153")
-declare -A cloud07=( ["em1"]="1160" ["em2"]="1161" ["em3"]="1162" ["em4"]="1163")
-declare -A cloud08=( ["em1"]="1170" ["em2"]="1171" ["em3"]="1172" ["em4"]="1173")
-declare -A cloud09=( ["em1"]="1180" ["em2"]="1181" ["em3"]="1182" ["em4"]="1183")
-declare -A cloud10=( ["em1"]="1190" ["em2"]="1191" ["em3"]="1192" ["em4"]="1193")
-declare -A cloud11=( ["em1"]="1200" ["em2"]="1201" ["em3"]="1202" ["em4"]="1203")
-declare -A cloud12=( ["em1"]="1210" ["em2"]="1211" ["em3"]="1212" ["em4"]="1213")
-declare -A cloud13=( ["em1"]="1220" ["em2"]="1221" ["em3"]="1222" ["em4"]="1223")
-declare -A cloud14=( ["em1"]="1230" ["em2"]="1231" ["em3"]="1232" ["em4"]="1233")
-declare -A cloud15=( ["em1"]="1240" ["em2"]="1241" ["em3"]="1242" ["em4"]="1243")
-declare -A cloud16=( ["em1"]="1250" ["em2"]="1251" ["em3"]="1252" ["em4"]="1253")
+declare -A offsets=( ["em1"]="0" ["em2"]="1" ["em3"]="2" ["em4"]="3")
 
 configdir=$data_dir/ports
 
@@ -111,123 +96,14 @@ for line in $(cat $configdir/$host_to_move); do
         exit 1
     fi
 
-    case $new_cloud in
-    cloud01)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud01["em1"]}
-        else
-            new_vlan=${cloud01[$interface]}
-        fi
-        ;;
-    cloud02)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud02["em1"]}
-        else
-            new_vlan=${cloud02[$interface]}
-        fi
-        ;;
-    cloud03)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud03["em1"]}
-        else
-            new_vlan=${cloud03[$interface]}
-        fi
-        ;;
-    cloud04)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud04["em1"]}
-        else
-            new_vlan=${cloud04[$interface]}
-        fi
-        ;;
-    cloud05)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud05["em1"]}
-        else
-            new_vlan=${cloud05[$interface]}
-        fi
-        ;;
-    cloud06)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud06["em1"]}
-        else
-            new_vlan=${cloud06[$interface]}
-        fi
-        ;;
-    cloud07)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud07["em1"]}
-        else
-            new_vlan=${cloud07[$interface]}
-        fi
-        ;;
-    cloud08)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud08["em1"]}
-        else
-            new_vlan=${cloud08[$interface]}
-        fi
-        ;;
-    cloud09)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud09["em1"]}
-        else
-            new_vlan=${cloud09[$interface]}
-        fi
-        ;;
-    cloud10)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud10["em1"]}
-        else
-            new_vlan=${cloud10[$interface]}
-        fi
-        ;;
-    cloud11)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud11["em1"]}
-        else
-            new_vlan=${cloud11[$interface]}
-        fi
-        ;;
-    cloud12)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud12["em1"]}
-        else
-            new_vlan=${cloud12[$interface]}
-        fi
-        ;;
-    cloud13)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud13["em1"]}
-        else
-            new_vlan=${cloud13[$interface]}
-        fi
-        ;;
-    cloud14)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud14["em1"]}
-        else
-            new_vlan=${cloud14[$interface]}
-        fi
-        ;;
-    cloud15)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud15["em1"]}
-        else
-            new_vlan=${cloud15[$interface]}
-        fi
-        ;;
-    cloud16)
-        if [ "$qinq" = "1" ]; then
-            new_vlan=${cloud16["em1"]}
-        else
-            new_vlan=${cloud16[$interface]}
-        fi
-        ;;
-    *)
-        echo unknown cloud $new_cloud
-        exit 1
-    esac
+    new_cloud_num=$(echo $new_cloud | sed 's/cloud//')
+    cloud_offset=$(expr $new_cloud_num \* 10)
+    base_vlan=$(expr 1090 + $cloud_offset)
+    if [ "$qinq" = "1" ]; then
+        new_vlan=$(expr $base_vlan + ${offsets["em1"]})
+    else
+        new_vlan=$(expr $base_vlan + ${offsets[$interface]})
+    fi
 
     if [ "$switch_type" == "ftos" ]; then
         # this needs some work
--- bin/quads-verify-switchconf.sh
+++ bin/quads-verify-switchconf.sh
@@ -59,22 +59,7 @@ fi
 
 echo $$ > $PIDFILE
 
-declare -A cloud01=( ["em1"]="1100" ["em2"]="1101" ["em3"]="1102" ["em4"]="1103")
-declare -A cloud02=( ["em1"]="1110" ["em2"]="1111" ["em3"]="1112" ["em4"]="1113")
-declare -A cloud03=( ["em1"]="1120" ["em2"]="1121" ["em3"]="1122" ["em4"]="1123")
-declare -A cloud04=( ["em1"]="1130" ["em2"]="1131" ["em3"]="1132" ["em4"]="1133")
-declare -A cloud05=( ["em1"]="1140" ["em2"]="1141" ["em3"]="1142" ["em4"]="1143")
-declare -A cloud06=( ["em1"]="1150" ["em2"]="1151" ["em3"]="1152" ["em4"]="1153")
-declare -A cloud07=( ["em1"]="1160" ["em2"]="1161" ["em3"]="1162" ["em4"]="1163")
-declare -A cloud08=( ["em1"]="1170" ["em2"]="1171" ["em3"]="1172" ["em4"]="1173")
-declare -A cloud09=( ["em1"]="1180" ["em2"]="1181" ["em3"]="1182" ["em4"]="1183")
-declare -A cloud10=( ["em1"]="1190" ["em2"]="1191" ["em3"]="1192" ["em4"]="1193")
-declare -A cloud11=( ["em1"]="1200" ["em2"]="1201" ["em3"]="1202" ["em4"]="1203")
-declare -A cloud12=( ["em1"]="1210" ["em2"]="1211" ["em3"]="1212" ["em4"]="1213")
-declare -A cloud13=( ["em1"]="1220" ["em2"]="1221" ["em3"]="1222" ["em4"]="1223")
-declare -A cloud14=( ["em1"]="1230" ["em2"]="1231" ["em3"]="1232" ["em4"]="1233")
-declare -A cloud15=( ["em1"]="1240" ["em2"]="1241" ["em3"]="1242" ["em4"]="1243")
-declare -A cloud16=( ["em1"]="1250" ["em2"]="1251" ["em3"]="1252" ["em4"]="1253")
+declare -A offsets=( ["em1"]="0" ["em2"]="1" ["em3"]="2" ["em4"]="3")
 
 if [ $# -ne 1 ]; then
     echo unexpected argument count.
@@ -102,120 +87,15 @@ for h in $($quads --cloud-only $env) ; do
         ifname=$(echo $interface | awk -F, '{ print $1 }')
         switchip=$(echo $interface | awk -F, '{ print $3 }')
         switchport=$(echo $interface | awk -F, '{ print $5 }')
-        case $env in
-            cloud01)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud01["em1"]}
-                else
-                    vlan=${cloud01[$ifname]}
-                fi
-                ;;
-            cloud02)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud02["em1"]}
-                else
-                    vlan=${cloud02[$ifname]}
-                fi
-                ;;
-            cloud03)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud03["em1"]}
-                else
-                    vlan=${cloud03[$ifname]}
-                fi
-                ;;
-            cloud04)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud04["em1"]}
-                else
-                    vlan=${cloud04[$ifname]}
-                fi
-                ;;
-            cloud05)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud05["em1"]}
-                else
-                    vlan=${cloud05[$ifname]}
-                fi
-                ;;
-            cloud06)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud06["em1"]}
-                else
-                    vlan=${cloud06[$ifname]}
-                fi
-                ;;
-            cloud07)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud07["em1"]}
-                else
-                    vlan=${cloud07[$ifname]}
-                fi
-                ;;
-            cloud08)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud08["em1"]}
-                else
-                    vlan=${cloud08[$ifname]}
-                fi
-                ;;
-            cloud09)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud09["em1"]}
-                else
-                    vlan=${cloud09[$ifname]}
-                fi
-                ;;
-            cloud10)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud10["em1"]}
-                else
-                    vlan=${cloud10[$ifname]}
-                fi
-                ;;
-            cloud11)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud11["em1"]}
-                else
-                    vlan=${cloud11[$ifname]}
-                fi
-                ;;
-            cloud12)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud12["em1"]}
-                else
-                    vlan=${cloud12[$ifname]}
-                fi
-                ;;
-            cloud13)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud13["em1"]}
-                else
-                    vlan=${cloud13[$ifname]}
-                fi
-                ;;
-            cloud14)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud14["em1"]}
-                else
-                    vlan=${cloud14[$ifname]}
-                fi
-                ;;
-            cloud15)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud15["em1"]}
-                else
-                    vlan=${cloud15[$ifname]}
-                fi
-                ;;
-            cloud16)
-                if [ "$qinq" = "1" ]; then
-                    vlan=${cloud16["em1"]}
-                else
-                    vlan=${cloud16[$ifname]}
-                fi
-                ;;
-        esac
+        env_num=$(echo $env | sed 's/cloud//')
+        env_offset=$(expr $env_num \* 10)
+        base_vlan=$(expr 1090 + $env_offset)
+        if [ "$qinq" = "1" ]; then
+            vlan=$(expr $base_vlan + ${offsets["em1"]})
+        else
+            vlan=$(expr $base_vlan + ${offsets[$interface]})
+        fi
+
         qinqsetting=$(ssh -o passwordauthentication=no -o connecttimeout=3 $switchip show configuration interfaces ${switchport} 2>/dev/null | sed 's/^apply-groups QinQ_vl\(.*\);/\1/g')
         vlanmember=$(ssh -o passwordauthentication=no -o connecttimeout=3 $switchip show vlans interface ${switchport}.0 2>/dev/null| egrep ^VLAN | sed 's/VLAN Name: vlan\(.*\), Index.*/\1/g')
         sleep 2
