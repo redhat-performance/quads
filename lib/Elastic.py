04e6921e8429e6224fff7a329f087a8fc38b1d62
Gonzalo Rafuls <grafuls@gmail.com>
2018-09-17 10:54:46 +0200

    Added logic to check for Foreman broken hosts before scheduling.
    This patch has the changes necessary for Quads to do a pre-check
    of hosts marked as broken on Foreman via hammer command. If the
    host is found to be broken Quads ignores the scheduling of such
    host and outputs an error message to the user.
    
    Fixes: https://github.com/redhat-performance/quads/issues/190
    Change-Id: I363bfda27302926b7bc8b85f518585776ee5807c

--- lib/Elastic.py
+++ lib/Elastic.py
@@ -1,12 +1,13 @@
 import elasticsearch
 import datetime
 
-class Elastic(object):
-    def __init__(self,elastichost, port=9200):
-        self.es = elasticsearch.Elasticsearch([{'host': elastichost,
-             'port': port}],send_get_body_as='POST')
 
-    def index(self,payload,index,_type='log') :
+class Elastic(object):
+    def __init__(self, elastichost, port=9200):
+        self.es = elasticsearch.Elasticsearch([{'host': elastichost,
+                                                'port': port}], send_get_body_as='POST')
+
+    def index(self, payload, index, _type='log'):
         now = datetime.datetime.utcnow()
         payload['timestamp'] = now
-        return self.es.index(index,_type,payload)
+        return self.es.index(index, _type, payload)
