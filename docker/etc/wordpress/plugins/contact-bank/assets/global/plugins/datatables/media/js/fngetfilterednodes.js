586e128dfaed29dfed6589e5ad663d7cfca4872c
Gonzalo Rafuls <grafuls@gmail.com>
2018-12-21 14:10:37 +0100

    Compressed wiki themes and plugins plus added entrypoint for uncompressin
    Change-Id: I3ce8045ab8c52481557ff2f297ca9688d66000b4

--- docker/etc/wordpress/plugins/contact-bank/assets/global/plugins/datatables/media/js/fngetfilterednodes.js
+++
@@ -1,8 +0,0 @@
-jQuery.fn.dataTableExt.oApi.fnGetFilteredNodes = function (oSettings) {
-   var anRows = [];
-   for (var i = 0, iLen = oSettings.aiDisplay.length; i < iLen; i++) {
-      var nRow = oSettings.aoData[oSettings.aiDisplay[i]].nTr;
-      anRows.push(nRow);
-   }
-   return anRows;
-};
--- docker/etc/wordpress/plugins/jetpack-markdown/markdown/lib/0-load.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if ( ! class_exists( 'MarkdownExtra_Parser' ) )
-	require_once('extra.php');
-
-require_once('gfm.php');
