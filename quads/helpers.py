f2ca467ce976456b4f1c54934402fc827bb46e52
Gonzalo Rafuls <grafuls@gmail.com>
2019-02-21 16:38:11 +0100

    [WIP] Move to badfish and general reafctoring to accomodate to the
    new mongo backend.
    
    Change-Id: I2f4b702e2292729e7069929cb0af8dee13072b9b

--- quads/helpers.py
+++ quads/helpers.py
@@ -1,22 +1,4 @@
-import yaml
-
-
-# Load QUADS yaml config
 from mongoengine import ObjectIdField
-
-
-def quads_load_config(quads_config):
-    try:
-        with open(quads_config, 'r') as config_file:
-            try:
-                quads_config_yaml = yaml.safe_load(config_file)
-            except yaml.YAMLError:
-                print("quads: Invalid YAML config: " + quads_config)
-                exit(1)
-    except Exception as ex:
-        print(ex)
-        exit(1)
-    return quads_config_yaml
 
 
 def param_check(data, params, defaults={}):
