e8e7e5b62c6a86c6f118ff899d0b11da1e0fc8d3
Gonzalo Rafuls <grafuls@redhat.com>
2019-03-19 11:30:14 -0400

    Fixes for simple table web.
    Replaced visualization images with color palette.
    
    Change-Id: Iec3616b56ac80be95b67f27ecd039be997a5f3ee

--- quads/tools/simple_table_generator.py
+++ quads/tools/simple_table_generator.py
@@ -6,98 +6,81 @@ import os
 import csv
 from datetime import datetime
 from jinja2 import Template
-from quads.config import conf as quads_config, API_URL, TEMPLATES_PATH
+from quads.config import conf, API_URL, TEMPLATES_PATH
+from quads.model import Cloud, Schedule, Host
 from quads.quads import Api
 
 
+def print_simple_table(data, data_colors, _days, _month, _year, _gentime, color_array):
+    lines = []
+    __days = []
+    for i, host in enumerate(data):
+        line = {"hostname": host.name}
+        __days = []
+        for j in range(0, _days):
+            chosen_color = data_colors[i][j-1]
+            cell_date = "%s-%.2d-%.2d 00:00" % (_year, _month, int(j+1))
+            _day = {
+                "day": j + 1,
+                "chosen_color": chosen_color,
+                "color": color_array[int(chosen_color) - 1],
+                "cell_date": cell_date,
+                "cell_time": datetime.strptime(cell_date, '%Y-%m-%d %H:%M')
+            }
+
+            cloud_history = Schedule.objects(host=host)
+            for schedule in cloud_history:
+                if schedule.start <= _day["cell_time"]:
+                    _cloud_obj = Cloud.objects(cloud=schedule.cloud)
+                    _day["display_description"] = _cloud_obj.description
+                    _day["display_owner"] = _cloud_obj.owner
+                    _day["display_ticket"] = _cloud_obj.ticket
+                break
+            __days.append(_day)
+
+        line["days"] = __days
+        lines.append(line)
+
+    with open(os.path.join(TEMPLATES_PATH, "simple_table")) as _file:
+        template = Template(_file.read())
+    content = template.render(
+        gentime=_gentime,
+        _days=_days,
+        lines=lines,
+    )
+
+    return content
+
+
 def generator(_host_file, _days, _month, _year, _gentime):
-    color_array = []
-
     quads = Api(API_URL)
-
-    def get_spaced_colors(visual_colors):
-        result = []
-        for k in sorted(visual_colors.keys()):
-            result.append(visual_colors[k].split())
-        return result
-
-    # covert palette to hex
-    def get_cell_color(a, b, c):
-        return "#" + \
-               ('0' if len(hex(int(a))) < 4 else '') + hex(int(a))[2:] + \
-               ('0' if len(hex(int(b))) < 4 else '') + hex(int(b))[2:] + \
-               ('0' if len(hex(int(c))) < 4 else '') + hex(int(c))[2:]
-
-    colors = get_spaced_colors(quads_config["visual_colors"])
-    for i in colors:
-        if int(i[0]) >= 0:
-            color_array.append(get_cell_color(i[0], i[1], i[2]))
-        else:
-            color_array.append(i[1])
-
-    def print_simple_table(data, data_colors, _days, _gentime):
-        lines = []
-        for item in range(len(data)):
-            line = {"hostname": str(data[item][0])}
-            __days = []
-            for j in range(1, _days):
-                chosen_color = data_colors[item][j - 1]
-                cell_date = "%s-%s-%.2d 00:00" % (_year, _month, int(j))
-                _day = {
-                    "day": j,
-                    "chosen_color": chosen_color,
-                    "color": color_array[int(chosen_color) - 1],
-                    "cell_date": cell_date,
-                    "cell_time": datetime.strptime(cell_date, '%Y-%m-%d %H:%M')
-                }
-
-                # TODO: this
-                # history = quads.get_history()
-                # for cloud in sorted(history["cloud" + str(_day["chosen_color"])]):
-                #     if datetime.fromtimestamp(cloud) <= _day["cell_time"]:
-                #         _day["display_description"] = \
-                #             history["cloud" + str(_day["chosen_color"])][cloud]["description"]
-                #         _day["display_owner"] = history["cloud" + str(_day["chosen_color"])][cloud]["owner"]
-                #         _day["display_ticket"] = history["cloud" + str(_day["chosen_color"])][cloud]["ticket"]
-                #         break
-                __days.append(_day)
-
-            line["days"] = __days
-            lines.append(line)
-
-        with open(os.path.join(TEMPLATES_PATH, "simple_table")) as _file:
-            template = Template(_file.read())
-        content = template.render(
-            gentime=_gentime,
-            _days=_days,
-            lines=lines,
-        )
-
-        return content
 
     if _host_file:
         with open(_host_file, 'r') as f:
             reader = csv.reader(f)
             your_list = list(reader)
     else:
-        your_list = []
-        for h in sorted(quads.get_hosts()):
-            your_list.append([h])
+        your_list = Host.objects()
     your_list_colors = []
-    for h in your_list:
+    for _host in your_list:
         one_host = []
-        for d in range(0, _days):
+        for d in range(0, _days - 1):
             day = d + 1
             if day < 10:
                 day_string = "0" + str(day)
             else:
                 day_string = str(day)
-            current = quads.get_current_schedule(host=h[0], date="{}-{}-{} 00:00".format(_year, _month, day_string))
-            if current:
-                one_host.append(current["cloud"].lstrip("cloud"))
+            current = quads.get_current_schedule(
+                host=_host.name, date="{}-{}-{} 00:00".format(_year, _month, day_string)
+            )
+            if current and "result" not in current:
+                _cloud_obj = Cloud.objects(id=current[0]["cloud"]["$oid"]).first()
+                one_host.append(_cloud_obj.name.lstrip("cloud"))
+            else:
+                one_host.append("01")
         your_list_colors.append(one_host)
 
-    return print_simple_table(your_list, your_list_colors, _days, _gentime)
+    return print_simple_table(your_list, your_list_colors, _days, _month, _year, _gentime, conf["visual_colors"])
 
 
 if __name__ == "__main__":
