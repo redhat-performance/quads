e17acff8672a03937284f2371f028f71019ded48
Gonzalo Rafuls <grafuls@redhat.com>
2019-03-21 18:13:07 -0400

    Fixes for make instack env.
    Removed logic for checking nullos variable.
    Instack env file is generated for all hosts in the cloud.
    
    Change-Id: I902bf2409a3de31ea98bbbdef8bd66be87a3d0cf

--- quads/tools/make_instackenv_json.py
+++ quads/tools/make_instackenv_json.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-
+import json
 import os
 import pathlib
-
-import requests
-
+from collections import defaultdict
 from datetime import datetime
+from quads.model import Cloud, Host
 from quads.tools.foreman import Foreman
-from quads.tools.csv_to_instack import csv_to_instack
-from quads.config import conf, API_URL
-from tempfile import NamedTemporaryFile
+from quads.config import conf
 
 
 def main():
@@ -19,15 +16,7 @@ def main():
         conf["foreman_password"]
     )
 
-    _cloud_response = requests.get(os.path.join(API_URL, "cloud"))
-    cloud_list = []
-    if _cloud_response.status_code == 200:
-        cloud_list = _cloud_response.json()
-
-    _host_response = requests.get(os.path.join(API_URL, "host"))
-    host_list = []
-    if _host_response.status_code == 200:
-        host_list = _host_response.json()
+    cloud_list = Cloud.objects()
 
     if not os.path.exists(conf["json_web_path"]):
         os.makedirs(conf["json_web_path"])
@@ -36,44 +25,40 @@ def main():
     for file in old_jsons:
         os.remove(os.path.join(conf["json_web_path"], file))
 
-    over_cloud = foreman.get_parametrized("params.%s" % conf["foreman_director_parameter"], "true")
-
-    columns = ["macaddress", "ipmi url", "ipmi user", "ipmi password", "ipmi tool"]
-    lines = []
     for cloud in cloud_list:
-        lines.append(",".join(columns))
+        host_list = Host.objects(cloud=cloud)
+
         foreman_password = conf["ipmi_password"]
-        if cloud["ticket"]:
-            foreman_password = cloud["ticket"]
+        if cloud.ticket:
+            foreman_password = cloud.ticket
 
+        json_data = defaultdict(list)
         for host in host_list:
-            is_overcloud = host["name"] in over_cloud.keys()
-            if is_overcloud:
-                mac = over_cloud[host]["mac"]
-                ipmi_url = "mgmt-%s" % host["name"]
-                ipmi_username = conf["ipmi_cloud_username"]
-                ipmi_tool = "pxe_ipmitool"
-                line = ",".join([mac, ipmi_url, ipmi_username, foreman_password, ipmi_tool])
-                lines.append(line)
+            host_data = foreman.get_idrac_host_with_details(host.name)
+            json_data['nodes'].append({
+                'pm_password': foreman_password,
+                'pm_type': "pxe_ipmitool",
+                'mac': [host_data["mac"]],
+                'cpu': "2",
+                'memory': "1024",
+                'disk': "20",
+                'arch': "x86_64",
+                'pm_user': conf["ipmi_cloud_username"],
+                'pm_addr': "mgmt-%s" % host.name})
 
-        with NamedTemporaryFile() as ntp:
-            for line in lines:
-                new_line = "%s\n" % line
-                ntp.write(bytes(new_line.encode("utf-8")))
-            ntp.seek(0)
-            content = csv_to_instack(ntp.name)
+        content = json.dumps(json_data, indent=4, sort_keys=True)
 
-            if not os.path.exists(conf["json_web_path"]):
-                pathlib.Path(conf["json_web_path"]).mkdir(parents=True, exist_ok=True)
+        if not os.path.exists(conf["json_web_path"]):
+            pathlib.Path(conf["json_web_path"]).mkdir(parents=True, exist_ok=True)
 
-            json_file = os.path.join(conf["json_web_path"], "%s_instackenv.json" % cloud['name'])
-            now = datetime.now()
-            if os.path.exists(json_file):
-                os.rename(json_file, "%s_%s" % (json_file, now.strftime("%Y-%m-%d_%H:%M:%S")))
-            with open(json_file, "w+") as _json_file:
-                _json_file.seek(0)
-                _json_file.write(content)
-            os.chmod(json_file, 644)
+        json_file = os.path.join(conf["json_web_path"], "%s_instackenv.json" % cloud.name)
+        now = datetime.now()
+        if os.path.exists(json_file):
+            os.rename(json_file, "%s_%s" % (json_file, now.strftime("%Y-%m-%d_%H:%M:%S")))
+        with open(json_file, "w+") as _json_file:
+            _json_file.seek(0)
+            _json_file.write(content)
+        os.chmod(json_file, 644)
 
 
 if __name__ == "__main__":
