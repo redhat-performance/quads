6fb96e6bdc241d4f0c4f60c6281310643c33f03e
Gonzalo Rafuls <grafuls@redhat.com>
2019-04-10 20:34:09 +0200

    Added public VLAN handler to verify switch conf.
    When we implemented this originally from 1.0 we didn't have handler
    for public vlans. Added this to accomodate to the necessary changes
    as per move_and_rebuild.
    
    Change-Id: I15367fb5439605f9f638d670d346a8a8243bb34d

--- quads/tools/verify_switchconf.py
+++ quads/tools/verify_switchconf.py
@@ -4,8 +4,9 @@ import logging
 
 from quads.config import API_URL, conf
 from quads.helpers import get_vlan
-from quads.model import Cloud, Host
+from quads.model import Cloud, Host, Vlan
 from quads.quads import Api
+from quads.tools.juniper_convert_port_public import juniper_convert_port_public
 from quads.tools.juniper_set_port import juniper_set_port
 from quads.tools.ssh_helper import SSHHelper
 
@@ -22,6 +23,8 @@ def verify(_cloud_name, change=False):
     for _host in hosts:
         _host_obj = Host.objects(name=_host["name"]).first()
         if _host_obj.interfaces:
+
+            _public_vlan_obj = Vlan.objects(cloud=_cloud_obj).first()
 
             for i, interface in enumerate(_host_obj.interfaces):
                 ssh_helper = SSHHelper(interface.ip_address, conf["junos_username"])
@@ -58,6 +61,9 @@ def verify(_cloud_name, change=False):
                 if int(old_vlan) != int(vlan):
                     logger.warning("interface %s not using QinQ_vl%s", interface.switch_port, vlan)
 
+                if _public_vlan_obj and i == len(_host_obj.interfaces) - 1:
+                    vlan = _public_vlan_obj.vlan_id
+
                 if int(vlan_member) != int(vlan):
                     logger.warning(
                         "interface %s appears to be a member of VLAN %s, should be %s",
@@ -68,21 +74,40 @@ def verify(_cloud_name, change=False):
 
                     if change:
                         logger.info('=== INFO: change requested')
-                        success = juniper_set_port(
-                            interface.ip_address,
-                            interface.switch_port,
-                            str(vlan_member),
-                            str(vlan)
-                        )
-                        if success:
-                            logger.info("Successfully updated switch settings.")
-                        else:
-                            logger.error(
-                                "There was something wrong updating switch for %s:%s" % (
-                                    _host["name"],
-                                    interface.name
+
+                        if _public_vlan_obj and i == len(_host_obj.interfaces) - 1:
+                            logger.info("Setting last interface to public vlan %s." % _public_vlan_obj.vlan_id)
+
+                            if int(old_vlan) != int(_public_vlan_obj.vlan_id):
+                                success = juniper_convert_port_public(
+                                    interface.ip_address,
+                                    interface.switch_port,
+                                    str(old_vlan),
+                                    str(vlan_member),
+                                    str(_public_vlan_obj.vlan_id)
                                 )
+                                if success:
+                                    logger.info("Successfully updated switch settings.")
+                                else:
+                                    logger.error(
+                                        "There was something wrong updating switch for %s:%s" % (_host.name, interface.name))
+                                    return False
+                        else:
+                            success = juniper_set_port(
+                                interface.ip_address,
+                                interface.switch_port,
+                                str(vlan_member),
+                                str(vlan)
                             )
+                            if success:
+                                logger.info("Successfully updated switch settings.")
+                            else:
+                                logger.error(
+                                    "There was something wrong updating switch for %s:%s" % (
+                                        _host["name"],
+                                        interface.name
+                                    )
+                                )
 
 
 if __name__ == "__main__":
