6bd936215b569c98f736b085533656f17cc52ee2
Gonzalo Rafuls <grafuls@redhat.com>
2019-04-18 22:44:01 +0200

    [WIP] Moving notifications to mongo.
    Before implementing these changes we need to make sure the following
    is addressed as changes are not backwards compatible.
    
    - Add provisioned field to all clouds
    - Insert notification doc for all clouds
    - Ticket to string field on VLAN doc
    - Need find a way to revert the flags when an assigment is extended
    
    Fixes:
    https://github.com/redhat-performance/quads/issues/233
    https://github.com/redhat-performance/quads/issues/232
    
    Change-Id: Ifdb16920f69180087709507e86a1b9820f763c8b

--- quads/api_v2.py
+++ quads/api_v2.py
@@ -111,7 +111,7 @@ class DocumentMethodHandler(MethodHandlerBase):
                         "owner": cloud.owner,
                         "ticket": cloud.ticket,
                         "ccuser": cloud.ccuser,
-                        "released": cloud.released
+                        "provisioned": cloud.provisioned
                     })
 
             return json.dumps(clouds_summary)
@@ -156,9 +156,8 @@ class DocumentMethodHandler(MethodHandlerBase):
                         "owner": cloud.owner,
                         "ticket": cloud.ticket,
                         "ccuser": cloud.ccuser,
-                        "released": cloud.released,
+                        "provisioned": cloud.provisioned,
                         "validated": cloud.validated,
-                        "notified": cloud.notified
                     })
 
             return json.dumps(clouds_summary)
@@ -213,6 +212,10 @@ class DocumentMethodHandler(MethodHandlerBase):
                             'Created %s %s' % (self.name, obj_name)
                         )
                     if self.name == "cloud":
+                        cloud_obj = self._get_obj(obj_name)
+                        notification_obj = model.Notification.objects(cloud=cloud_obj, ticket=data["ticket"]).first()
+                        if not notification_obj:
+                            model.Notification(cloud=cloud_obj, ticket=data["ticket"]).save()
 
                         if _vlan:
                             update_data = {}
---
+++ quads/tools/451493_accomodate.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from quads.model import Notification, Cloud, Vlan
+
+if __name__ == "__main__":
+    clouds = Cloud.objects()
+    for cloud in clouds:
+        # Insert notification doc for all clouds
+        Notification(cloud=cloud, ticket=cloud.ticket).save()
+        if cloud.ticket != "000000":
+            # Add provisioned field to all clouds
+            cloud.update(provisioned=cloud.released)
+
+    vlans = Vlan.objects()
+    for vlan in vlans:
+        # Ticket to string field on VLAN doc
+        vlan.update(ticket="000000")
