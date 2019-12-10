71cd4decb9b839e3d64fc3cc30c83fcf8bfb335d
Will Foster <wfoster@redhat.com>
2019-12-10 16:52:08 +0000

    WIP:  feat: add reporting tool to generate webpage.
    * Tool to pull in data from quads-cli --report-available
    * Generate into HTML / table
    * Run out of cron once an hour or whatever makes sense.
    * Toggle executable bit on reports.py (unrelated)
    
    TODO:
    
    * Replace using a flat file temp-report-data.txt once format is right
    * Pull data from QUADS reports library
    * Fix iteration to pull in all values, currently stuck on only pulling
    in the first line of systems info.
    
    How to test:
    
    cd /opt/quads/tools/
    python3 report_overview.py > /tmp/test.html
    
    fixes: https://github.com/redhat-performance/quads/issues/301
    Change-Id: I8d57ec213cb51b373ebbe1c1cf86b32573aa54a5

---
+++ quads/tools/report_overview.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# generate simple report from quads-cli data
+# TODO:  reference reports.py / QUADS data here instead of temp-report-data.txt
+# TODO:  fix iteration issues, only one line of systems is reporting
+# TODO:  Ideally this should look like this:
+#        http://us.funcamp.net:9999/et.html
+#        https://funcamp.net/w/et.html
+# WIP REMOVE ME
+header_row = """
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#f0f0f0;}
+.tg .tg-edkc{font-size:28px;vertical-align:top}
+.tg .tg-24i8{font-size:24px;vertical-align:top}
+</style>
+<table class="tg">
+<html>
+<table>
+<tr>
+<th class="tg-edkc">Server Type</th>
+<th class="tg-edkc">Total</th>
+<th class="tg-edkc">Free</th>
+<th class="tg-edkc">Scheduled</th>
+<th class="tg-edkc">2 Weeks Free</th>
+<th class="tg-edkc">4 Weeks Free</th>
+</tr>
+"""
+
+# FIX ME
+# DO not use temp-report-data.txt
+# Use QUADS or reports library here
+
+def genoverview():
+    with open("temp-report-data.txt") as f:
+        lines = f.readlines()
+        for line in lines[3:-1]:
+            Type = line.split("|")
+            servertype = Type[0]
+            total = Type[1]
+            free = Type[2]
+            sched = Type[3]
+            twoweek = Type[4]
+            fourweek = Type[5]
+            tabledata = ("<tr><td class='tg-24i8'>" +servertype + "</td>\n"
+                         + "<td class='tg-24i8'>" +total + "</td>\n"
+                         + "<td class='tg-24i8'>" +free + "</td>\n"
+                         + "<td class='tg-24i8'>" +sched + "</td>\n"
+                         + "<td class='tg-24i8'>" +twoweek + "</td>\n"
+                         + "<td class='tg-24i8'>" +fourweek + "</td></tr>\n"
+                        )
+        rawtable = (header_row + tabledata)
+        fulltable = (rawtable + "</table></html")
+
+    print(fulltable)
+
+
+genoverview()
