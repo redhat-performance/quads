f8b8374c9f34ae3870df2cd209fd407361c1df96
Gonzalo Rafuls <grafuls@redhat.com>
2019-12-12 06:06:29 -0500

    WIP: feat: detailed report
    Change-Id: Ia1cd5ec6d56b887ee14d8a8d829dc6819bd2959f

--- quads/tools/reports.py
+++ quads/tools/reports.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 from quads.helpers import date_span, first_day_month, last_day_month, month_delta_past, date_to_object_id
-from quads.model import Schedule, Host, CloudHistory
+from quads.model import Schedule, Host, CloudHistory, Cloud, Q
 from datetime import datetime, timedelta
 
 
@@ -147,6 +147,51 @@ def process_scheduled(_logger, month, now):
         f"{hosts:>8}| "
         f"{utilization:>10}%| "
     )
+
+
+def report_detailed(_logger, _start, _end):
+    start = _start.replace(hour=21, minute=59, second=0)
+    start_defer = start - timedelta(weeks=1)
+    start_defer_id = date_to_object_id(start_defer)
+    end = _end.replace(hour=22, minute=1, second=0)
+    end_id = date_to_object_id(end)
+    cloud_history = CloudHistory.objects(
+        __raw__={
+            "_id": {
+                "$lt": end_id,
+                "$gt": start_defer_id,
+            },
+        }
+    ).order_by("-_id")
+
+    headers = ["Owner", "Ticket", "Cloud", "Description", "Systems", "Scheduled", "Duration"]
+    _logger.info(
+        f"{headers[0]:<9}| "
+        f"{headers[1]:>9}| "
+        f"{headers[2]:>8}| "
+        f"{headers[3]:>10}| "
+        f"{headers[4]:>5}| "
+        f"{headers[5]:>6}| "
+        f"{headers[6]:>5}| "
+    )
+
+    for cloud in cloud_history:
+        cloud_ref = Cloud.objects(name=cloud.name).first()
+        schedule = Schedule.objects(
+            Q(end__lt=end) & Q(start__gt=start) & Q(cloud=cloud_ref)
+        ).order_by("-_id")
+        if schedule:
+            delta = schedule[0].end - schedule[0].start
+            description = cloud.description[:len(headers[3])]
+            _logger.info(
+                f"{cloud.owner:<9}| "
+                f"{cloud.ticket:>9}| "
+                f"{cloud.name:>8}| "
+                f"{description:>11}| "
+                f"{schedule.count():>7}| "
+                f"{str(schedule[0].start)[:10]:>9}| "
+                f"{delta.days:>7}| "
+            )
 
 
 if __name__ == "__main__":
