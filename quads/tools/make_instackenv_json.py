d41ee837e5fb81d443238ff59dbcb7495099cafe
Sai Sindhur Malleni <smalleni@redhat.com>
2020-04-14 19:16:49 -0400

    Add an inventory file for OpenSHift Baremetal Deployments
    OpenShift Baremetal deployments require two MACs to be specified
    per host. The current instackenv.json provides only one MAC (provisioning)
    per host. It would be possible to add another mac into the list of macs
    for each host in instackenv.json itself, but it is unclear how this affects
    existing OpenStack deployment automation (especially with SuperMicros).
    This commit adds the ability to serve another inventory file, with just a
    minor modification of having two MACs per host. The adavntage with having a
    separate ocpinventory.json file is that automation could consume it and save time
    as well as we now have the ability to further enhance this file in the future
    to accomodate more OpenShift specific items. This commit acts as a bridge between
    QUADS and automation to deploy OpenShift on Baremetal.
    
    Change-Id: Ib88ee205d59dab627d18aa952f4f809f312e744d

--- quads/tools/make_instackenv_json.py
+++ quads/tools/make_instackenv_json.py
@@ -14,7 +14,7 @@ from quads.config import conf
 
 
 def main():
-    if conf["openstack_management"]:
+    if conf["openstack_management"] or conf["openshift_management"]:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         foreman = Foreman(
@@ -42,7 +42,8 @@ def main():
             if cloud.ticket:
                 foreman_password = f"{conf['infra_location']}@{cloud.ticket}"
 
-            json_data = defaultdict(list)
+            osp_data = defaultdict(list)
+            ocp_data = defaultdict(list)
             for host in host_list:
                 if conf["foreman_unavailable"]:
                     overcloud = {"result": "true"}
@@ -57,37 +58,62 @@ def main():
                     _overcloud_result = overcloud["result"]
 
                 if "result" in overcloud and _overcloud_result:
-                    mac = "00:00:00:00:00:00"
-                    if len(host.interfaces) > 1:
-                        mac = host.interfaces[1].mac_address
-                    json_data['nodes'].append({
-                        'pm_password': foreman_password,
-                        'pm_type': "pxe_ipmitool",
-                        'mac': [mac],
-                        'cpu': "2",
-                        'memory': "1024",
-                        'disk': "20",
-                        'arch': "x86_64",
-                        'pm_user': conf["ipmi_cloud_username"],
-                        'pm_addr': "mgmt-%s" % host.name})
+                    if conf["openstack_management"]
+                        mac = "00:00:00:00:00:00"
+                        if len(host.interfaces) > 1:
+                            mac = host.interfaces[1].mac_address
+                        osp_data['nodes'].append({
+                            'pm_password': foreman_password,
+                            'pm_type': "pxe_ipmitool",
+                            'mac': [mac],
+                            'cpu': "2",
+                            'memory': "1024",
+                            'disk': "20",
+                            'arch': "x86_64",
+                            'pm_user': conf["ipmi_cloud_username"],
+                            'pm_addr': "mgmt-%s" % host.name})
+                    if conf["openshift_management"]
+                        mac = []
+                        if len(host.interfaces) > 2:
+                            for i in range(0,2):
+                            mac.append(host.interfaces[i].mac_address)
+                        ocp_data['nodes'].append({
+                            'pm_password': foreman_password,
+                            'pm_type': "pxe_ipmitool",
+                            'mac': mac,
+                            'cpu': "2",
+                            'memory': "1024",
+                            'disk': "20",
+                            'arch': "x86_64",
+                            'pm_user': conf["ipmi_cloud_username"],
+                            'pm_addr': "mgmt-%s" % host.name})
 
-            content = json.dumps(json_data, indent=4, sort_keys=True)
+            osp_content = json.dumps(osp_data, indent=4, sort_keys=True)
+            ocp_content = json.dumps(ocp_data, indent=4, sort_keys=True)
 
             if not os.path.exists(conf["json_web_path"]):
                 pathlib.Path(conf["json_web_path"]).mkdir(parents=True, exist_ok=True)
 
             now = datetime.now()
-            new_json_file = os.path.join(
+            osp_new_json_file = os.path.join(
                 conf["json_web_path"],
                 "%s_instackenv.json_%s" % (cloud.name, now.strftime("%Y-%m-%d_%H:%M:%S"))
             )
-            json_file = os.path.join(conf["json_web_path"], "%s_instackenv.json" % cloud.name)
-            with open(new_json_file, "w+") as _json_file:
-                _json_file.seek(0)
-                _json_file.write(content)
-            os.chmod(new_json_file, 0o644)
-            copyfile(new_json_file, json_file)
-
+            ocp_new_json_file = os.path.join(
+                conf["json_web_path"],
+                "%s_ocpinventory.json_%s" % (cloud.name, now.strftime("%Y-%m-%d_%H:%M:%S"))
+            )
+            osp_json_file = os.path.join(conf["json_web_path"], "%s_instackenv.json" % cloud.name)
+            ocp_json_file = os.path.join(conf["json_web_path"], "%s_ocpinventory.json" % cloud.name)
+            with open(osp_new_json_file, "w+") as _osp_json_file, (ocp_new_json_file, "w+") as _osp_json_file:
+                _osp_json_file.seek(0)
+                _osp_json_file.write(osp_content)
+                _ocp_json_file.seek(0)
+                _ocp_json_file.write(ocp_content)
+            os.chmod(osp_new_json_file, 0o644)
+            copyfile(osp_new_json_file, osp_json_file)
+            os.chmod(ocp_new_json_file, 0o644)
+            copyfile(ocp_new_json_file, ocp_json_file)
 
 if __name__ == "__main__":
     main()
