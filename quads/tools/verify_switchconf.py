0f9c9dec60c8f1eeac9c53ecbf57c124201fe397
Gonzalo Rafuls <grafuls@redhat.com>
2020-06-02 14:18:11 +0200

    fix: vlan setup for last nic
    we were wrongly comparing vlan id to vlan object for setting vlans
    on last nics.
    
    closes: https://github.com/redhat-performance/quads/issues/330
    Change-Id: I34c23ead671c1642fe18712ae9c93750992cd1e7

--- quads/tools/verify_switchconf.py
+++ quads/tools/verify_switchconf.py
@@ -3,10 +3,9 @@
 import argparse
 import logging
 
-from quads.config import API_URL, conf
+from quads.config import conf
 from quads.helpers import get_vlan
-from quads.model import Cloud, Host, Vlan
-from quads.quads import Api
+from quads.model import Cloud, Host
 from quads.tools.juniper_convert_port_public import juniper_convert_port_public
 from quads.tools.juniper_set_port import juniper_set_port
 from quads.tools.ssh_helper import SSHHelper
@@ -17,25 +16,20 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 def verify(_cloud_name, change=False):
     _cloud_obj = Cloud.objects(name=_cloud_name).first()
-    quads = Api(API_URL)
+    hosts = Host.objects(cloud=_cloud_obj)
 
-    try:
-        hosts = quads.get_cloud_hosts(_cloud_name)
-    except ConnectionError:
-        logger.error("Could not connect to the quads-server, verify service is up and running.")
-        exit(1)
-
-    for _host in hosts:
-        _host_obj = Host.objects(name=_host["name"]).first()
+    for _host_obj in hosts:
         if _host_obj.interfaces:
-            interfaces = sorted(_host_obj.interfaces, key=lambda k: k['name'])
+            interfaces = sorted(_host_obj.interfaces, key=lambda k: k["name"])
             for i, interface in enumerate(interfaces):
                 ssh_helper = SSHHelper(interface.ip_address, conf["junos_username"])
                 last_nic = i == len(_host_obj.interfaces) - 1
                 vlan = get_vlan(_cloud_obj, i, last_nic)
 
                 try:
-                    old_vlan_out = ssh_helper.run_cmd("show configuration interfaces %s" % interface.switch_port)
+                    old_vlan_out = ssh_helper.run_cmd(
+                        "show configuration interfaces %s" % interface.switch_port
+                    )
                     old_vlan = old_vlan_out[0].split(";")[0].split()[1]
                     if old_vlan.startswith("QinQ"):
                         old_vlan = old_vlan[7:]
@@ -43,13 +37,21 @@ def verify(_cloud_name, change=False):
                     old_vlan = None
 
                 try:
-                    vlan_member_out = ssh_helper.run_cmd("show configuration vlans | display set | match %s.0" % interface.switch_port)
+                    vlan_member_out = ssh_helper.run_cmd(
+                        "show configuration vlans | display set | match %s.0"
+                        % interface.switch_port
+                    )
                     vlan_member = vlan_member_out[0].split()[2][4:].strip(",")
                 except IndexError:
                     if not _cloud_obj.vlan and not last_nic:
                         logger.warning(
                             "Could not determine the previous VLAN member for %s on %s, switch %s, switchport %s"
-                            % (_host["name"], interface.name, interface.ip_address, interface.switch_port)
+                            % (
+                                _host_obj.name,
+                                interface.name,
+                                interface.ip_address,
+                                interface.switch_port,
+                            )
                         )
                     vlan_member = None
 
@@ -59,12 +61,19 @@ def verify(_cloud_name, change=False):
                     if not _cloud_obj.vlan and not last_nic:
                         logger.warning(
                             "Could not determine the previous VLAN for %s on %s, switch %s, switchport %s"
-                            % (_host["name"], interface.name, interface.ip_address, interface.switch_port)
+                            % (
+                                _host_obj.name,
+                                interface.name,
+                                interface.ip_address,
+                                interface.switch_port,
+                            )
                         )
                     old_vlan = get_vlan(_cloud_obj, i, last_nic)
 
                 if int(old_vlan) != int(vlan):
-                    logger.warning("interface %s not using QinQ_vl%s", interface.switch_port, vlan)
+                    logger.warning(
+                        "interface %s not using QinQ_vl%s", interface.switch_port, vlan
+                    )
 
                 if _cloud_obj.vlan and i == len(_host_obj.interfaces) - 1:
                     vlan = _cloud_obj.vlan.vlan_id
@@ -75,45 +84,57 @@ def verify(_cloud_name, change=False):
                             "interface %s appears to be a member of VLAN %s, should be %s",
                             interface.switch_port,
                             vlan_member,
-                            vlan
+                            vlan,
                         )
 
                     if change:
-                        logger.info('=== INFO: change requested')
+                        logger.info("=== INFO: change requested")
 
-                        if _cloud_obj.vlan and _cloud_obj.vlan != old_vlan and last_nic:
-                            logger.info("Setting last interface to public vlan %s." % _cloud_obj.vlan.vlan_id)
+                        if (
+                            _cloud_obj.vlan
+                            and int(_cloud_obj.vlan.vlan_id) != int(old_vlan)
+                            and last_nic
+                        ):
+                            logger.info(
+                                "Setting last interface to public vlan %s."
+                                % _cloud_obj.vlan.vlan_id
+                            )
 
                             success = juniper_convert_port_public(
                                 interface.ip_address,
                                 interface.switch_port,
                                 old_vlan,
-                                vlan
+                                vlan,
                             )
                         else:
                             success = juniper_set_port(
                                 interface.ip_address,
                                 interface.switch_port,
                                 vlan_member,
-                                vlan
+                                vlan,
                             )
 
                         if success:
                             logger.info("Successfully updated switch settings.")
                         else:
                             logger.error(
-                                "There was something wrong updating switch for %s:%s" % (
-                                    _host_obj.name,
-                                    interface.name
-                                )
+                                "There was something wrong updating switch for %s:%s"
+                                % (_host_obj.name, interface.name)
                             )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Query current cloud for a given host')
-    parser.add_argument('--cloud', dest='cloud', type=str, default=None,
-                        help='Cloud name to verify switch configuration for.')
-    parser.add_argument('--change', dest='change', action='store_true', help='Commit changes on switch.')
+    parser = argparse.ArgumentParser(description="Query current cloud for a given host")
+    parser.add_argument(
+        "--cloud",
+        dest="cloud",
+        type=str,
+        default=None,
+        help="Cloud name to verify switch configuration for.",
+    )
+    parser.add_argument(
+        "--change", dest="change", action="store_true", help="Commit changes on switch."
+    )
 
     args = parser.parse_args()
     verify(args.cloud, args.change)
