c30bdfd6372d09562a6258c72ee35d231aedbf8c
Gonzalo Rafuls <grafuls@redhat.com>
2020-07-28 17:40:28 +0200

    WIP: web front end flask example for ls available
    with model filter
    
    Change-Id: I34b430ea7eafd6e1b5c821708d00bd0639c9f51a

---
+++ web/forms.py
@@ -0,0 +1,15 @@
+# forms.py
+
+from wtforms import SelectField, DateField, validators
+from flask_wtf import FlaskForm
+from quads.config import conf
+
+
+class ModelSearchForm(FlaskForm):
+    models_list = conf.get("models").split(",")
+    models_choices = []
+    for model in models_list:
+        models_choices.append((model, model))
+    model = SelectField('Models:', choices=models_choices)
+    start = DateField('Start at', format="%m/%d/%Y", validators=[validators.data_required()])
+    end = DateField('End at', format="%m/%d/%Y", validators=[validators.data_required()])
