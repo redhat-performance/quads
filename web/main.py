45a47b27bcffa8f906f05d1e153957778879b1bd
Gonzalo Rafuls <grafuls@redhat.com>
2020-07-28 17:40:28 +0200

    WIP: web front end flask example for ls available
    with model filter
    
    Change-Id: I34b430ea7eafd6e1b5c821708d00bd0639c9f51a

---
+++ web/main.py
@@ -0,0 +1,58 @@
+# main.py
+import asyncio
+
+from app import app
+from forms import ModelSearchForm
+from flask import flash, render_template, request, redirect
+
+from quads.config import conf
+from quads.model import Host, Schedule
+from quads.tools.foreman import Foreman
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    search = ModelSearchForm(request.form)
+    if request.method == 'POST':
+        return search_results(search)
+
+    return render_template('index.html', form=search)
+
+
+@app.route('/results')
+def search_results(search):
+    results = []
+    model = search.data['model']
+
+    if search.data['model'] != '':
+        _filter = {"model": model}
+        hosts = Host.objects(**_filter).all()
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    foreman = Foreman(
+        conf["foreman_api_url"],
+        conf["foreman_username"],
+        conf["foreman_password"],
+        loop=loop,
+    )
+    broken_hosts = loop.run_until_complete(foreman.get_broken_hosts())
+
+    available = []
+    for host in hosts:
+        if Schedule.is_host_available(
+            host=host["name"], start=search.data['start'], end=search.data['end']
+        ) and not broken_hosts.get(host["name"], False):
+            available.append(host["name"])
+
+    if not available:
+        flash('No results found!')
+        return redirect('/')
+    else:
+        # display results
+        return render_template('results.html', results=results)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5001)
