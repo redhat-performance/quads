ddae34915752e9a406ec7b95f5e132ba987f2211
Gonzalo Rafuls <grafuls@redhat.com>
2020-07-30 17:58:55 +0200

    feat: smarter datepickers plus multi select modes
    Change-Id: I7a6c9ffb82a4961a8170356fe69aba2171b4c380

--- web/forms.py
+++ web/forms.py
@@ -1,15 +1,15 @@
 # forms.py
 
-from wtforms import SelectField, DateField, validators
+from wtforms import DateField, validators, SelectMultipleField
 from flask_wtf import FlaskForm
 from quads.config import conf
 
 
 class ModelSearchForm(FlaskForm):
     models_list = conf.get("models").split(",")
-    models_choices = [("All", "All")]
+    models_choices = []
     for model in models_list:
         models_choices.append((model, model))
-    model = SelectField('Models:', choices=models_choices)
+    model = SelectMultipleField('Models:', choices=models_choices)
     start = DateField('Start at', format="%m/%d/%Y", validators=[validators.data_required()])
     end = DateField('End at', format="%m/%d/%Y", validators=[validators.data_required()])
