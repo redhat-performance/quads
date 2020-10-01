d511099f77ecef7aeb961b1e93192b5084349c55
Gonzalo Rafuls <grafuls@redhat.com>
2020-10-01 15:38:07 +0200

    feat: asynchronous netcat and validate_env
    Change-Id: I65cad5b7d9f0fb98641316a303d2e960fd420fd5

---
+++ quads/tools/helpers.py
@@ -0,0 +1,10 @@
+import asyncio
+
+
+def get_running_loop() -> asyncio.AbstractEventLoop:
+    loop = asyncio.get_event_loop()
+    if not loop.is_running():
+        raise RuntimeError(
+            "The object should be created within an async function"
+        )
+    return loop
