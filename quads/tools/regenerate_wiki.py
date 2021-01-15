481588648e6e37bc4ccdd1142357cefdef9661f1
Gonza Rafuls <grafuls@redhat.com>
2021-01-15 14:35:56 +0100

    fix: handling for missing --add-schedule or --mod-schedule
    additional fix for badfish on get_reset_types
    additional fix for --host listing 2 clouds when date arg
    is passed.
    fix for extend/shrink on non current/future allocations
    
    closes: https://github.com/redhat-performance/quads/issues/356
    closes: https://github.com/redhat-performance/quads/issues/355
    Change-Id: Ida59b01f742b0f57d1c64ff636c2dcf7726c6e13

--- quads/tools/regenerate_wiki.py
+++ quads/tools/regenerate_wiki.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-
+import logging
 import os
+
 from datetime import datetime
+from xmlrpc.client import ProtocolError
 from git import Repo, InvalidGitRepositoryError
 from quads.config import conf
 from quads.tools import create_input, create_input_assignments, racks_wiki
@@ -16,6 +18,8 @@ wp_wiki_assignments_title = conf["wp_wiki_assignments_title"]
 wp_wiki_assignments_page_id = conf["wp_wiki_assignments_page_id"]
 wp_wiki_git_manage = conf["wp_wiki_git_manage"]
 wp_wiki_git_repo_path = conf["wp_wiki_git_repo_path"]
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
 
@@ -32,14 +36,17 @@ if __name__ == "__main__":
                 repo.index.commit("%s content update" % datetime.now().strftime("%a %b %d %T %Y"))
                 repo.remotes.origin.push()
 
-    racks_wiki.update_wiki(
-        url="http://%s/xmlrpc.php" % wp_wiki,
-        username=wp_username,
-        password=wp_password,
-        _page_title=wp_wiki_main_title,
-        _page_id=wp_wiki_main_page_id,
-        _markdown=main_md,
-    )
+    try:
+        racks_wiki.update_wiki(
+            url="http://%s/xmlrpc.php" % wp_wiki,
+            username=wp_username,
+            password=wp_password,
+            _page_title=wp_wiki_main_title,
+            _page_id=wp_wiki_main_page_id,
+            _markdown=main_md,
+        )
+    except ProtocolError as ex:
+        logger.error(ex.errmsg)
 
     create_input_assignments.main()
     assignments_md = os.path.join(wp_wiki_git_repo_path, "assignments.md")
@@ -55,13 +62,16 @@ if __name__ == "__main__":
                 repo.index.commit("%s content update" % datetime.now().strftime("%a %b %d %T %Y"))
                 repo.remotes.origin.push()
 
-    racks_wiki.update_wiki(
-        url="http://%s/xmlrpc.php" % wp_wiki,
-        username=wp_username,
-        password=wp_password,
-        _page_title=wp_wiki_assignments_title,
-        _page_id=wp_wiki_assignments_page_id,
-        _markdown=assignments_md,
-    )
+    try:
+        racks_wiki.update_wiki(
+            url="http://%s/xmlrpc.php" % wp_wiki,
+            username=wp_username,
+            password=wp_password,
+            _page_title=wp_wiki_assignments_title,
+            _page_id=wp_wiki_assignments_page_id,
+            _markdown=assignments_md,
+        )
+    except ProtocolError as ex:
+        logger.error(ex.errmsg)
 
     regenerate_vlans_wiki()
