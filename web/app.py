514410e93397329773b939437290ae7cfb5b504f
tsvoboda <tsvoboda@redhat.com>
2021-04-06 12:38:12 +0200

    feat: quads-web Flask behind gunicorn and nginx
    Nginx:
    Added nginx configuration in quads/web/nginx, though nginx's confs
    generally aren't just drag n' drop setups as there is always at least
    something that has to be manually configured (see 'server_name'), this
    makes packaging a bit harder as enabling the nginx quads-web service
    from the point of package installation is close to impossible.
    
    Unless there is some other way I am not aware of for nginx@RHEL
    (debian-based has /etc/nginx/sites-available/sites-enabled for this exact purpose) then
    the package should place an example configuration into some
    /usr/doc/... location and document the rest of the proccess elsewhere
    for the user to setup & fill-in the rest.
    
    Also from what it seems unless quads-web does/starts doing all the web work,
    adding nginx is not really worth it, gunicorn itself will completely suffice
    
    Quads-web:
    It would be nice to vendor in the python3-flask-bootstrap package (eg. extract the
    jinja templates and static bootstrap files) because:
    - Nginx is way better at handling static stuff
      Right now the nginx config has a bit of a hack (L:19 L:24) to propagate /static/ urls to the flask itself.
      - Having static stuff within one location also allows fine-grained control of for example (static) content caching.
    - Removes a package level dependency that is hardly going to change
      unless we really need something new & shiny from bootstrap (which
      could be done just now when vendoring)
    
    Change-Id: I26b603eb92d79e61abae3008094c00891f20437b

--- web/app.py
+++ web/app.py
@@ -3,7 +3,9 @@
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
 
 app = Flask(__name__)
-app.secret_key = "flask rocks!"
 Bootstrap(app)
+
+app.config.from_object(Config)
