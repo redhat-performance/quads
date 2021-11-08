1595618273b431d2916f1fde2652da03e9179c24
tsvoboda <tsvoboda@redhat.com>
2021-11-08 19:19:28 +0100

    feat: Refactoring
    CLI:
        - bin/quads-cli broken apart into quads/cli
        - bin/quads-cli:main() refactored into quads/cli/cli:QuadsCli class
        - all the cli command functions are defined on QuadsCli as methods
          instead of if/elif step in the main() function
        - argparser slightly restructured
    
    Config:
        - quads/config.py refactored into Config singleton class
        - This class combines both globals (attributes of the Config class)
          and values loaded from yaml (populated as attributes)
        - For now it allows for both attribute and getitem/subscription
          access, though only one should eventually be preferred to decrease
          the surface of possible errors
        - Added tests to verify all above
    
    Change-Id: Ic113bd50453d3bfb6d722359ddb5cf40bfb64c49

--- quads/config.py
+++ quads/config.py
@@ -1,46 +1,125 @@
 #!/usr/bin/env python3
 import logging
 import os
+
 import yaml
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_CONF_PATH = os.path.join(os.path.dirname(__file__), "../conf/quads.yml")
 
-def quads_load_config(quads_config):
+
+class _ConfigBase:
+    def load_from_yaml(self, filepath: str = DEFAULT_CONF_PATH):
+        """
+        Load values from yaml file as attributes of this class.
+        Will never override existing attributes.
+        """
+        with open(filepath, "r") as config_file:
+            conf = yaml.safe_load(config_file)
+            assert type(conf) is dict
+
+            for key, value in conf.items():
+                if hasattr(self, key):
+                    logger.warning(f"Key '{key}' is already defined on config class, not overriding")
+                    continue
+                setattr(self, key, value)
+
+            logger.debug(f"Loaded yaml config from '{filepath}'")
+
+    def __getitem__(self, item: str):
+        """
+        Allow acces thru subscription:
+
+        Config['key'] === Config.key
+        Config['QUADS_VERSION'] === Config.QUADS_VERSION
+
+        This should eventually be removed, having two
+        ways to access one thing does not sound safe.
+        """
+        try:
+            return getattr(self, item)
+        except AttributeError as attr_exc:
+            raise KeyError() from attr_exc
+
+
+class _Config(_ConfigBase):
+    """
+    Configuration "singleton"
+
+    Class defined values should be considered globals (capitalized)
+    and they always override values set in yaml.
+
+    Examples:
+        Config.QUADS_VERSION
+        Config.email_host
+    """
+
+    LOGFMT = "%(asctime)-12s : %(levelname)-8s - %(message)s"
+    STDFMT = "- %(levelname)-8s - %(message)s"
+
+    API = "v2"
+
+    @property
+    def API_URL(self):
+        return os.path.join(self.quads_base_url, "api", self.API)
+
+    FPING_TIMEOUT = 10000
+
+    QUADS_VERSION = "1.1.5"
+    QUADS_CODENAME = "gaúcho"
+
+    SUPPORTED = [
+        "fc640",
+        "r620",
+        "r630",
+        "r640",
+        "640",
+        "r650",
+        "650",
+        "r720",
+        "r730xd",
+        "r930",
+        "r730",
+        "r740xd",
+        "740xd",
+        "r720xd",
+        "7425",
+        "7525",
+    ]
+
+    OFFSETS = {"em1": 0, "em2": 1, "em3": 2, "em4": 3, "em5": 4}
+    TEMPLATES_PATH = os.path.join(os.path.dirname(__file__), "templates")
+    INTERFACES = {
+        "em1": ["172.16", "172.21"],
+        "em2": ["172.17", "172.22"],
+        "em3": ["172.18", "172.23"],
+        "em4": ["172.19", "172.24"],
+        "em5": ["172.20", "172.25"],
+    }
+
+
+# Making sure there is exactly one instance of config used elsewhere
+Config = _Config()
+
+if __name__ == "__main__":
+    # quick debug
+    logging.basicConfig(level=logging.DEBUG)
+
+    logger.info("Loading config file")
+    Config.load_from_yaml(DEFAULT_CONF_PATH)
+    logger.info("File loaded ok")
+
+    logger.debug(Config.API_URL)
+    logger.debug(Config["API_URL"])
+
+    # these should raise
     try:
-        with open(quads_config, 'r') as config_file:
-            try:
-                quads_config_yaml = yaml.safe_load(config_file)
-            except yaml.YAMLError as ex:
-                logger.debug(ex)
-                logger.error("quads: Invalid YAML config: " + quads_config)
-                return
-    except Exception as ex:
-        logger.debug(ex)
-        return
-    return quads_config_yaml
+        _ = Config.NOT_EXISTS
+    except Exception as exc:
+        logger.info(exc, exc_info=exc)
 
-
-quads_config_file = os.path.join(os.path.dirname(__file__), "../conf/quads.yml")
-conf = quads_load_config(quads_config_file)
-
-QUADSVERSION = "1.1.5"
-QUADSCODENAME = "gaúcho"
-SUPPORTED = ["fc640", "r620", "r630", "r640", "640", "r650", "650", "r720", "r730xd", "r930", "r730", "r740xd", "740xd", "r720xd", "7425", "7525" ]
-
-OFFSETS = {"em1": 0, "em2": 1, "em3": 2, "em4": 3, "em5": 4}
-TEMPLATES_PATH = os.path.join(os.path.dirname(__file__), "templates")
-INTERFACES = {
-    "em1": ["172.16", "172.21"],
-    "em2": ["172.17", "172.22"],
-    "em3": ["172.18", "172.23"],
-    "em4": ["172.19", "172.24"],
-    "em5": ["172.20", "172.25"],
-}
-LOGFMT = "%(asctime)-12s : %(levelname)-8s - %(message)s"
-STDFMT = "- %(levelname)-8s - %(message)s"
-
-API = "v2"
-API_URL = os.path.join(conf['quads_base_url'], 'api', API)
-
-FPING_TIMEOUT = 10000
+    try:
+        _ = Config["NOT_EXISTS_GETITEM"]
+    except Exception as exc:
+        logger.info(exc, exc_info=exc)
