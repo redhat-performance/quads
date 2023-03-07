02bbf1889cf65e22739fabba952f83914f64e31f
Gonza Rafuls <grafuls@redhat.com>
2023-03-07 15:34:28 +0100

    feat: added --cloud filter to --ls-available
    Closes: https://github.com/redhat-performance/quads/issues/432
    
    Change-Id: Ib2dcea57e2cfd1d22743d5f6e0a0e4933b981338

--- quads/cli/cli.py
+++ quads/cli/cli.py
@@ -488,6 +488,13 @@ class QuadsCli:
         _start = datetime.strptime(self.cli_args["schedstart"], "%Y-%m-%d %H:%M")
         _end = datetime.strptime(self.cli_args["schedend"], "%Y-%m-%d %H:%M")
 
+        if self.cli_args["cloud"]:
+            cloud = Cloud.objects(name=self.cli_args["cloud"]).first()
+            if cloud:
+                kwargs["cloud"] = cloud
+            else:
+                raise CliException(f"Cloud {self.cli_args['cloud']} not found.")
+
         available = []
         current = []
         all_hosts = Host.objects(**kwargs).all()
@@ -1721,7 +1728,7 @@ class QuadsCli:
             ).isoformat()
         schedules = Schedule.current_schedule(**_kwargs)
         if schedules:
-            _kwargs = {}
+            _kwargs = {"retired": False}
             if self.cli_args["filter"]:
                 filter_args = self._filter_kwargs(self.cli_args["filter"])
                 _kwargs.update(filter_args)
