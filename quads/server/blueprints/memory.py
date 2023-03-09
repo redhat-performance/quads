49bdf4ea07f862643aa963ec4b6f7b9762bacf82
Dominik Vagner <dvagner@redhat.com>
2023-03-09 11:34:59 +0100

    feat: created tests for the Flask API
    Added docker setup for use in Jenkins.
    
    Change-Id: If20f0480c8a0e98405b3181e9adeee2d525b90fe

---
+++ quads/server/blueprints/memory.py
@@ -0,0 +1,127 @@
+import json
+
+from flask import Blueprint, jsonify, request, Response
+
+from quads.server.blueprints import check_access
+from quads.server.dao.host import HostDao
+from quads.server.dao.memory import MemoryDao
+from quads.server.models import db, Memory
+
+memory_bp = Blueprint("memory", __name__)
+
+
+@memory_bp.route("/")
+def get_all_memory() -> Response:
+    _memories = MemoryDao.get_memories()
+    return jsonify([_memory.as_dict() for _memory in _memories])
+
+
+@memory_bp.route("/<hostname>")
+def get_memory(hostname: str) -> Response:
+    _host = HostDao.get_host(hostname)
+    if not _host:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": f"Host not found: {hostname}",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    return jsonify([_memory.as_dict() for _memory in _host.memory])
+
+
+@memory_bp.route("/<hostname>", methods=["POST"])
+@check_access("admin")
+def create_memory(hostname: str) -> Response:
+    _host = HostDao.get_host(hostname)
+    if not _host:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": f"Host not found: {hostname}",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    data = request.get_json()
+
+    handle = data.get("handle")
+    size_gb = data.get("size_gb")
+
+    if not handle:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": "Missing argument: handle",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    memories = MemoryDao.get_memory_for_host(_host.id)
+    if any(memory.handle == handle for memory in memories):
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": f"Memory with this handle ({handle}) already exists for this host.",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    if not size_gb:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": "Missing argument: size_gb",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    if not size_gb > 0:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": "Argument can't be negative or zero: size_gb",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    _memory_obj = Memory(handle=handle, size_gb=size_gb, host_id=_host.id)
+    db.session.add(_memory_obj)
+    db.session.commit()
+    return jsonify(_memory_obj.as_dict())
+
+
+@memory_bp.route("/<hostname>", methods=["DELETE"])
+@check_access("admin")
+def delete_memory(hostname: str) -> Response:
+    _host = HostDao.get_host(hostname)
+    if not _host:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": f"Host not found: {hostname}",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    data = request.get_json()
+
+    memory_id = data.get("id")
+    if not memory_id:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": "Missing argument: id",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    _memory_obj = MemoryDao.get_memory(memory_id)
+    if not _memory_obj:
+        response = {
+            "status_code": 400,
+            "error": "Bad Request",
+            "message": f"Memory not found: {memory_id}",
+        }
+        return Response(response=json.dumps(response), status=400)
+
+    db.session.delete(_memory_obj)
+    db.session.commit()
+    response = {
+        "status_code": 201,
+        "message": "Memory deleted",
+    }
+    return Response(response=json.dumps(response), status=201)
---
+++ tests/api/test_clouds.py
@@ -0,0 +1,164 @@
+from datetime import datetime
+
+from tests.helpers import unwrap_json
+
+
+class TestCreateClouds:
+    def test_valid(self, test_client, auth):
+        """
+        | GIVEN: Client with defaults in database and user logged in
+        | WHEN: User tries to create a cloud with valid data, loop 10 times
+        | THEN: User should be able to create a cloud
+        """
+        auth_header = auth.get_auth_header()
+        for cloud_id in range(1, 11):
+            cloud_name = f"cloud{str(cloud_id).zfill(2)}"
+            response = unwrap_json(test_client.post(
+                "/api/v3/clouds",
+                json=dict(name=cloud_name),
+                headers=auth_header,
+            ))
+            assert response.status_code == 200
+            assert response.json["id"] == cloud_id
+            assert response.json["name"] == cloud_name
+            duration = datetime.utcnow() - datetime.strptime(response.json["last_redefined"],
+                                                             "%a, %d %b %Y %H:%M:%S GMT")
+            assert duration.total_seconds() < 5
+
+    def test_invalid_missing_arg(self, test_client, auth):
+        """
+        | GIVEN: Client with defaults in database and user logged in
+        | WHEN: User tries to create a cloud with missing name
+        | THEN: User should not be able to create a cloud
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.post(
+            "/api/v3/clouds",
+            json=dict(),
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Missing argument: name"
+
+    def test_invalid_exists(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to create a cloud with already an existing cloud using the same name
+        | THEN: User should not be able to create a cloud
+        """
+        auth_header = auth.get_auth_header()
+        cloud_name = "cloud01"
+        response = unwrap_json(test_client.post(
+            "/api/v3/clouds",
+            json=dict(name=cloud_name),
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Cloud {cloud_name} already exists"
+
+
+class TestReadClouds:
+    def test_valid_single(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to read a cloud with valid id
+        | THEN: User should be able to read a cloud
+        """
+        auth_header = auth.get_auth_header()
+        cloud_id = 1
+        cloud_name = f"cloud{str(cloud_id).zfill(2)}"
+        response = unwrap_json(test_client.get(
+            f"/api/v3/clouds/{cloud_name}",
+            headers=auth_header,
+        ))
+        assert response.status_code == 200
+        assert response.json["id"] == cloud_id
+        assert response.json["name"] == cloud_name
+        assert response.json["last_redefined"] is not None
+
+    def test_valid_multiple(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to read all clouds
+        | THEN: User should be able to read all clouds
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.get(
+            "/api/v3/clouds",
+            headers=auth_header,
+        ))
+        assert response.status_code == 200
+        assert len(response.json) == 10
+        for cloud_id in range(1, 11):
+            cloud_name = f"cloud{str(cloud_id).zfill(2)}"
+            assert response.json[cloud_id - 1]["id"] == cloud_id
+            assert response.json[cloud_id - 1]["name"] == cloud_name
+            assert response.json[cloud_id - 1]["last_redefined"] is not None
+
+    def test_invalid_not_found_single(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to read a cloud with invalid id
+        | THEN: User should not be able to read a cloud
+        """
+        auth_header = auth.get_auth_header()
+        cloud_name = "cloud11"
+        response = unwrap_json(test_client.get(
+            f"/api/v3/clouds/{cloud_name}",
+            headers=auth_header,
+        ))
+        assert response.json == {}
+
+
+class TestDeleteClouds:
+    def test_valid(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to delete a cloud with valid id
+        | THEN: User should be able to delete a cloud
+        """
+        auth_header = auth.get_auth_header()
+        cloud_id = 1
+        cloud_name = f"cloud{str(cloud_id).zfill(2)}"
+        response = unwrap_json(test_client.delete(
+            f"/api/v3/clouds",
+            json=dict(name=cloud_name),
+            headers=auth_header,
+        ))
+        assert response.status_code == 201
+        assert response.json["message"] == f"Cloud {cloud_name} deleted"
+
+    def test_invalid_missing_arg(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to delete a cloud while passing no name argument
+        | THEN: User should not be able to delete a cloud
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.delete(
+            "/api/v3/clouds",
+            json=dict(),
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Missing argument: name"
+
+    def test_invalid_cloud_not_found(self, test_client, auth):
+        """
+        | GIVEN: Clouds from test_valid in database and user logged in
+        | WHEN: User tries to delete a cloud with id that doesn't match any cloud
+        | THEN: User should not be able to delete a cloud
+        """
+        auth_header = auth.get_auth_header()
+        cloud_name = "cloud11"
+        response = unwrap_json(test_client.delete(
+            "/api/v3/clouds",
+            json=dict(name=cloud_name),
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Cloud not found: {cloud_name}"
