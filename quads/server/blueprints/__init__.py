26426791119227a16b982ca13db112d8f1fed988
Dominik Vagner <dvagner@redhat.com>
2023-03-09 11:34:59 +0100

    feat: created tests for the Flask API
    Added docker setup for use in Jenkins.
    
    Change-Id: If20f0480c8a0e98405b3181e9adeee2d525b90fe

---
+++ quads/server/blueprints/__init__.py
@@ -0,0 +1,69 @@
+import json
+from functools import wraps
+from flask import request, abort, Response
+from quads.server.models import User, db, Role
+
+
+def check_access(role):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs) -> Response:
+            if "Authorization" in request.headers:
+                auth_value = request.headers["Authorization"].split(" ")
+                if len(auth_value) < 2:
+                    response = {
+                        "message": "Authorization header malformed",
+                        "error": "Bad Request",
+                    }
+                    return Response(response=json.dumps(response), status=400)
+
+                if auth_value[0].lower() == "bearer":
+                    try:
+                        username = User.decode_auth_token(auth_value[1])
+                        current_user = (
+                            db.session.query(User)
+                            .filter(User.email == username)
+                            .first()
+                        )
+                        if current_user is None:
+                            response = {
+                                "message": "Invalid Authentication token!",
+                                "error": "Unauthorized",
+                            }
+                            return Response(response=json.dumps(response), status=401)
+                        if not current_user.active:
+                            abort(403)
+                    except Exception as e:
+                        response = {
+                            "message": "Something went wrong",
+                            "error": str(e),
+                        }
+                        return Response(response=json.dumps(response), status=500)
+
+                if auth_value[0].lower() == "basic":
+                    username = request.authorization["username"]
+                    password = request.authorization["password"]
+                    current_user = (
+                        db.session.query(User).filter(User.email == username).first()
+                    )
+                    if not current_user.verify_password(password):
+                        response = {
+                            "message": "Invalid password",
+                            "error": "Unauthorized",
+                        }
+                        return Response(response=json.dumps(response), status=401)
+
+                role_obj = db.session.query(Role).filter(Role.name == role).first()
+                if role_obj not in current_user.roles:
+                    return abort(401)
+            else:
+                response = {
+                    "message": "Missing authentication data",
+                    "error": "Bad Request",
+                }
+                return Response(response=json.dumps(response), status=400)
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
---
+++ quads/server/dao/disk.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from quads.server.dao.baseDao import BaseDao
+from quads.server.models import db, Disk
+
+
+class DiskDao(BaseDao):
+    @staticmethod
+    def get_disks() -> List[Disk]:
+        disks = db.session.query(Disk).all()
+        return disks
+
+    @staticmethod
+    def get_disk(disk_id: int) -> Disk:
+        clouds = db.session.query(Disk).filter(Disk.id == disk_id).first()
+        return clouds
