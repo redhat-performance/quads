1cd5ee017ecb350f9765a2b97bf191c765652248
Dominik Vagner <dvagner@redhat.com>
2023-03-09 11:34:59 +0100

    feat: created tests for the Flask API
    Added docker setup for use in Jenkins.
    
    Change-Id: If20f0480c8a0e98405b3181e9adeee2d525b90fe

---
+++ quads/server/dao/baseDao.py
@@ -0,0 +1,110 @@
+from sqlalchemy import and_, or_
+
+from quads.server.models import db, Interface, Disk, Memory, Processor
+from quads.server import models
+from flask import current_app
+from sqlalchemy.exc import SQLAlchemyError
+
+FILTERING_OPERATORS = {
+    "==": "eq",
+    "!=": "ne",
+    ">": "gt",
+    "<": "lt",
+    ">=": "ge",
+    "<=": "le",
+    "like": "like",
+    "ilike": "ilike",
+    "in": "in",
+}
+OPERATORS = {
+    "__ne": "!=",
+    "__lt": "<",
+    "__lte": "<=",
+    "__gt": ">",
+    "__gte": ">=",
+    "__like": "like",
+    "__ilike": "ilike",
+    "__in": "in",
+}
+MAP_MODEL = {
+    "interfaces": Interface,
+    "disks": Disk,
+    "memory": Memory,
+    "processors": Processor,
+}
+
+
+class EntryNotFound(Exception):
+    pass
+
+
+class BaseDao:
+    @staticmethod
+    def safe_commit() -> bool:
+        """
+        Safely attempt to commit changes to MySQL.  Rollback in case of a failure.
+        :return: True if the commit was successful, False if a rollback occurred.
+        """
+        try:
+            db.session.commit()
+            current_app.logger.info("SQL Safely Committed")
+            return True
+        except SQLAlchemyError as error:
+            db.session.rollback()
+            current_app.logger.error("SQL Commit Failed!  Rolling back...")
+            current_app.logger.error(error.args)
+            return False
+
+    @classmethod
+    def create_query_select(cls, model, filters=None, columns=None):
+        query_columns = cls.create_query_columns(model=model, columns=columns)
+        query = db.session.query(*query_columns).distinct(model.id)
+        for expression in filters:
+            try:
+                column_name, op, value = expression
+            except ValueError:
+                raise Exception("Invalid filter: %s" % expression)
+            if op not in FILTERING_OPERATORS:
+                raise Exception("Invalid filter operation: %s" % op)
+            attrs = column_name.split(".")
+            if len(attrs) > 1:
+                column_name = attrs[0]
+                parent_column = MAP_MODEL[column_name]
+                column = getattr(parent_column, attrs[1])
+                query = query.filter(model.id == parent_column.host_id)
+            else:
+                column = getattr(model, column_name, None)
+            if not column:
+                raise Exception("Invalid filter column: %s" % column_name)
+            else:
+                try:
+                    attr = (
+                        list(
+                            filter(
+                                lambda e: hasattr(column, e % FILTERING_OPERATORS[op]),
+                                ["%s", "%s_", "__%s__"],
+                            )
+                        )[0]
+                        % FILTERING_OPERATORS[op]
+                    )
+                except IndexError:
+                    raise Exception(
+                        "Invalid filter operator: %s" % FILTERING_OPERATORS[op]
+                    )
+                if value == "null":
+                    value = None
+                query = query.filter(getattr(column, attr)(value))
+        return query.all()
+
+    @classmethod
+    def create_query_columns(cls, model, columns):
+        if not columns:
+            return [model]
+
+        cols = []
+        for column in columns:
+            attr = getattr(model, column, None)
+            if not attr:
+                raise Exception("Invalid column name %s" % column)
+            cols.append(attr)
+        return cols
---
+++ tests/api/test_interfaces.py
@@ -0,0 +1,299 @@
+import pytest
+
+from tests.helpers import unwrap_json
+from tests.config import (
+    INTERFACE_1_REQUEST,
+    INTERFACE_1_RESPONSE,
+    INTERFACE_1_UPDATE_REQUEST,
+    INTERFACE_1_UPDATE_RESPONSE,
+)
+
+prefill_settings = ["clouds, hosts"]
+
+
+class TestCreateInterfaces:
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_host(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to create an interface for an invalid host
+        | THEN: Interface should not be created
+        """
+        auth_header = auth.get_auth_header()
+        host_name = "invalid_host"
+        response = unwrap_json(test_client.post(
+            f"/api/v3/interfaces/{host_name}",
+            json=INTERFACE_1_REQUEST[0],
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Host not found: {host_name}"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_missing_arg(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to create an interface with missing argument
+        | THEN: Interface should not be created
+        """
+        auth_header = auth.get_auth_header()
+        interface_request = INTERFACE_1_REQUEST[0].copy()
+        del interface_request["name"]
+        response = unwrap_json(test_client.post(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=interface_request,
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Missing argument: name"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_speed(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to create an interface with invalid speed (<= 0)
+        | THEN: Interface should not be created
+        """
+        auth_header = auth.get_auth_header()
+        interface_request = INTERFACE_1_REQUEST[0].copy()
+        interface_request["speed"] = -1
+        response = unwrap_json(test_client.post(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=interface_request,
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Argument can't be negative or zero: speed"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_valid(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to create an interface with valid data
+        | THEN: Interface should be created
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.post(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=INTERFACE_1_REQUEST[0],
+            headers=auth_header,
+        ))
+        assert response.status_code == 200
+        assert response.json == INTERFACE_1_RESPONSE
+
+
+class TestReadInterfaces:
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_host(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to read interfaces for an invalid host
+        | THEN: Interface should not be read
+        """
+        auth_header = auth.get_auth_header()
+        host_name = "invalid_host"
+        response = unwrap_json(test_client.get(
+            f"/api/v3/interfaces/{host_name}",
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Host not found: {host_name}"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_valid(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to read interfaces for a valid host
+        | THEN: Interface should be read
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.get(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            headers=auth_header,
+        ))
+        assert response.status_code == 200
+        assert response.json == [INTERFACE_1_RESPONSE]
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_valid_no_interfaces(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to read interfaces for a valid host with no interfaces
+        | THEN: Interface should be read
+        """
+        auth_header = auth.get_auth_header()
+        host_name = "host2.example.com"
+        response = unwrap_json(test_client.get(
+            f"/api/v3/interfaces/{host_name}",
+            headers=auth_header,
+        ))
+        assert response.status_code == 200
+        assert response.json == []
+
+
+class TestUpdateInterfaces:
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_host(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to update an interface for an invalid host
+        | THEN: Interface should not be updated
+        """
+        auth_header = auth.get_auth_header()
+        host_name = "invalid_host"
+        response = unwrap_json(test_client.patch(
+            f"/api/v3/interfaces/{host_name}",
+            json=INTERFACE_1_REQUEST[0],
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Host not found: {host_name}"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_missing_id(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to update an interface without specifying an ID for the interface.
+        | THEN: Interface should not be updated
+        """
+        auth_header = auth.get_auth_header()
+        update_request = INTERFACE_1_UPDATE_REQUEST.copy()
+        del update_request["id"]
+        response = unwrap_json(test_client.patch(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=update_request,
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Missing argument: id"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_wrong_id(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to update an interface with an ID that doesn't match any interface.
+        | THEN: Interface should not be updated
+        """
+        auth_header = auth.get_auth_header()
+        update_request = INTERFACE_1_UPDATE_REQUEST.copy()
+        update_request["id"] = 42
+        response = unwrap_json(test_client.patch(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=update_request,
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Interface not found: {update_request['id']}"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_valid(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to update an interface with valid data
+        | THEN: Interface should be updated
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.patch(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=INTERFACE_1_UPDATE_REQUEST,
+            headers=auth_header,
+        ))
+        assert response.status_code == 200
+        assert response.json == INTERFACE_1_UPDATE_RESPONSE
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_speed(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to update an interface with invalid speed
+        | THEN: Interface should not be updated
+        """
+        auth_header = auth.get_auth_header()
+        update_request = INTERFACE_1_UPDATE_REQUEST.copy()
+        update_request["speed"] = -1
+        response = unwrap_json(test_client.patch(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json=update_request,
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Argument can't be negative or zero: speed"
+
+
+class TestDeleteInterfaces:
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_host(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to delete an interface for an invalid host
+        | THEN: Interface should not be deleted
+        """
+        auth_header = auth.get_auth_header()
+        host_name = "invalid_host"
+        response = unwrap_json(test_client.delete(
+            f"/api/v3/interfaces/{host_name}",
+            json=INTERFACE_1_REQUEST[0],
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Host not found: {host_name}"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_missing_id(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to delete an interface without specifying an ID for the interface.
+        | THEN: Interface should not be deleted
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.delete(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json={},
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == "Missing argument: id"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_invalid_wrong_id(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to delete an interface with an ID that doesn't match any interface.
+        | THEN: Interface should not be deleted
+        """
+        auth_header = auth.get_auth_header()
+        invalid_id = 42
+        response = unwrap_json(test_client.delete(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json={"id": invalid_id},
+            headers=auth_header,
+        ))
+        assert response.status_code == 400
+        assert response.json["error"] == "Bad Request"
+        assert response.json["message"] == f"Interface not found: {invalid_id}"
+
+    @pytest.mark.parametrize("prefill", prefill_settings, indirect=True)
+    def test_valid(self, test_client, auth, prefill):
+        """
+        | GIVEN: Defaults, auth token, clouds and hosts
+        | WHEN: User tries to delete an interface with valid data
+        | THEN: Interface should be deleted
+        """
+        auth_header = auth.get_auth_header()
+        response = unwrap_json(test_client.delete(
+            f"/api/v3/interfaces/{INTERFACE_1_REQUEST[1]}",
+            json={"id": INTERFACE_1_RESPONSE["id"]},
+            headers=auth_header,
+        ))
+        assert response.status_code == 201
+        assert response.json["message"] == "Interface deleted"
