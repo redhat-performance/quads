195bd1b3752e247c0e48afd60f761541dee97d2e
Thirumalesh Aaraveti <athiruma@redhat.com>
2023-12-13 12:29:22 +0530

    Added the pytests
    Change-Id: I037fdcf2b971b4dc67feb272791456b307522841

---
+++ tests/tools/test_logger.py
@@ -0,0 +1,22 @@
+import unittest
+import logging
+from unittest.mock import patch
+
+from quads.tools.logger import ColorFormatter
+
+
+class TestColorFormatter(unittest.TestCase):
+
+    @patch('sys.stdout.isatty')
+    def test_format_tty(self, mock_isatty):
+        mock_isatty.return_value = True
+        log_record = logging.LogRecord("TestLogger", logging.DEBUG, "test.py", 42, "Debug message", None, None)
+        formatted_output = ColorFormatter().format(log_record)
+        assert formatted_output == "\x1b[32;21mDebug message\x1b[0m"
+
+    @patch('sys.stdout.isatty')
+    def test_format_not_tty(self, mock_isatty):
+        mock_isatty.return_value = False
+        log_record = logging.LogRecord("TestLogger", logging.DEBUG, "test.py", 42, "Debug message", None, None)
+        formatted_output = ColorFormatter().format(log_record)
+        assert formatted_output == "Debug message"
