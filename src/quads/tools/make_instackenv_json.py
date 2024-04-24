02838ba9b00c078245274ddee473de3499cb96b1
Gonzalo Rafuls <grafuls@redhat.com>
2024-04-24 17:55:00 +0200

    web: make instack on demand
    Change-Id: Ibf78f9c90af2e26bdb3ab15c58a875c65deca225

--- src/quads/tools/make_instackenv_json.py
+++ src/quads/tools/make_instackenv_json.py
@@ -4,11 +4,8 @@ import asyncio
 import json
 import os
 import time
-import pathlib
 from collections import defaultdict
 from distutils.util import strtobool
-from datetime import datetime
-from shutil import copyfile
 
 from quads.quads_api import QuadsApi
 from quads.tools.external.foreman import Foreman
@@ -17,7 +14,7 @@ from quads.config import Config
 quads = QuadsApi(Config)
 
 
-async def make_env_json(filename):
+async def make_env_json(filename, cloud):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     foreman = Foreman(
@@ -27,100 +24,59 @@ async def make_env_json(filename):
         loop=loop,
     )
 
-    cloud_list = quads.get_clouds()
-
-    if not os.path.exists(Config["json_web_path"]):
-        os.makedirs(Config["json_web_path"])
-
     now = time.time()
     old_jsons = [file for file in os.listdir(Config["json_web_path"]) if ":" in file]
     for file in old_jsons:
-        if (
-            os.stat(os.path.join(Config["json_web_path"], file)).st_mtime
-            < now - Config["json_retention_days"] * 86400
-        ):
+        if os.stat(os.path.join(Config["json_web_path"], file)).st_mtime < now - Config["json_retention_days"] * 86400:
             os.remove(os.path.join(Config["json_web_path"], file))
 
-    for cloud in cloud_list:
-        host_list = quads.filter_hosts({"cloud": cloud.name})
-        assignment = quads.get_active_cloud_assignment(cloud.name)
-        foreman_password = Config["ipmi_password"]
-        if assignment and assignment.ticket:
-            foreman_password = f"{Config['infra_location']}@{assignment.ticket}"
+    host_list = quads.filter_hosts({"cloud": cloud.name})
+    assignment = quads.get_active_cloud_assignment(cloud.name)
+    foreman_password = Config["ipmi_password"]
+    if assignment and assignment.ticket:
+        foreman_password = f"{Config['infra_location']}@{assignment.ticket}"
 
-        data = defaultdict(list)
-        for host in host_list:
-            if Config["foreman_unavailable"]:
-                overcloud = {"result": "true"}
-            else:
-                overcloud = loop.run_until_complete(
-                    foreman.get_host_param(host.name, "overcloud")
-                )
+    data = defaultdict(list)
+    for host in host_list:
+        if Config["foreman_unavailable"]:
+            overcloud = {"result": "true"}
+        else:
+            overcloud = loop.run_until_complete(foreman.get_host_param(host.name, "overcloud"))
 
-            if not overcloud:
-                overcloud = {"result": "true"}
+        if not overcloud:
+            overcloud = {"result": "true"}
 
-            if type(overcloud["result"]) != bool:
-                try:
-                    _overcloud_result = strtobool(overcloud["result"])
-                except ValueError:
-                    print(f"WARN: {host.name} overcloud value is not set correctly.")
-                    _overcloud_result = 1
-            else:
-                _overcloud_result = overcloud["result"]
+        if type(overcloud["result"]) != bool:
+            try:
+                _overcloud_result = strtobool(overcloud["result"])
+            except ValueError:
+                print(f"WARN: {host.name} overcloud value is not set correctly.")
+                _overcloud_result = 1
+        else:
+            _overcloud_result = overcloud["result"]
 
-            if "result" in overcloud and _overcloud_result:
-                mac = []
-                if filename == "instackenv":
-                    for interface in sorted(host.interfaces, key=lambda k: k.name):
-                        if interface.pxe_boot:
-                            mac.append(interface.mac_address)
-                if filename == "ocpinventory":
-                    mac = [
-                        interface.mac_address
-                        for interface in sorted(host.interfaces, key=lambda k: k.name)
-                    ]
-                data["nodes"].append(
-                    {
-                        "name": host.name,
-                        "pm_password": foreman_password,
-                        "pm_type": "pxe_ipmitool",
-                        "mac": mac,
-                        "cpu": "2",
-                        "memory": "1024",
-                        "disk": "20",
-                        "arch": "x86_64",
-                        "pm_user": Config["ipmi_cloud_username"],
-                        "pm_addr": "mgmt-%s" % host.name,
-                    }
-                )
+        if "result" in overcloud and _overcloud_result:
+            mac = []
+            if filename == "instackenv":
+                for interface in sorted(host.interfaces, key=lambda k: k.name):
+                    if interface.pxe_boot:
+                        mac.append(interface.mac_address)
+            if filename == "ocpinventory":
+                mac = [interface.mac_address for interface in sorted(host.interfaces, key=lambda k: k.name)]
+            data["nodes"].append(
+                {
+                    "name": host.name,
+                    "pm_password": foreman_password,
+                    "pm_type": "pxe_ipmitool",
+                    "mac": mac,
+                    "cpu": "2",
+                    "memory": "1024",
+                    "disk": "20",
+                    "arch": "x86_64",
+                    "pm_user": Config["ipmi_cloud_username"],
+                    "pm_addr": "mgmt-%s" % host.name,
+                }
+            )
 
         content = json.dumps(data, indent=4, sort_keys=True)
-
-        if not os.path.exists(Config["json_web_path"]):
-            pathlib.Path(Config["json_web_path"]).mkdir(parents=True, exist_ok=True)
-
-        now = datetime.now()
-        new_json_file = os.path.join(
-            Config["json_web_path"],
-            "%s_%s.json_%s" % (cloud.name, filename, now.strftime("%Y-%m-%d_%H:%M:%S")),
-        )
-        json_file = os.path.join(
-            Config["json_web_path"], "%s_%s.json" % (cloud.name, filename)
-        )
-        with open(new_json_file, "w+") as _json_file:
-            _json_file.seek(0)
-            _json_file.write(content)
-        os.chmod(new_json_file, 0o644)
-        copyfile(new_json_file, json_file)
-
-
-def main():
-    if Config["openstack_management"]:
-        asyncio.get_event_loop().run_until_complete(make_env_json("instackenv"))
-    if Config["openshift_management"]:
-        asyncio.get_event_loop().run_until_complete(make_env_json("ocpinventory"))
-
-
-if __name__ == "__main__":
-    main()
+        return content
