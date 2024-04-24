13f4769bafbe338d855fe4d5304ad37c76660281
Gonzalo Rafuls <grafuls@redhat.com>
2024-04-24 17:55:00 +0200

    web: make instack on demand
    Change-Id: Ibf78f9c90af2e26bdb3ab15c58a875c65deca225

--- tests/tools/test_instackenv.py
+++ tests/tools/test_instackenv.py
@@ -2,41 +2,23 @@
 import os
 
 from quads.config import Config
-from quads.tools.make_instackenv_json import main
+from quads.tools.make_instackenv_json import make_env_json
 from tests.tools.test_base import TestBase
 
 
 class TestInstackenv(TestBase):
-    def test_make_instackenv_json(self):
+    async def test_make_instackenv_json(self):
         Config.__setattr__("openstack_management", True)
         Config.__setattr__("foreman_unavailable", True)
-        Config.__setattr__(
-            "json_web_path", os.path.join(os.path.dirname(__file__), "artifacts/")
-        )
-        main()
-        assert list(
-            open(
-                os.path.join(
-                    os.path.dirname(__file__), "artifacts/cloud99_instackenv.json"
-                )
-            )
-        ) == list(
+        cloud99_instackenv = await make_env_json("instackenv", "cloud99")
+        assert list(cloud99_instackenv) == list(
             open(os.path.join(os.path.dirname(__file__), "fixtures/cloud99_env.json"))
         )
 
-    def test_make_ocpinventory_json(self):
+    async def test_make_ocpinventory_json(self):
         Config.__setattr__("openshift_management", True)
         Config.__setattr__("foreman_unavailable", True)
-        Config.__setattr__(
-            "json_web_path", os.path.join(os.path.dirname(__file__), "artifacts/")
-        )
-        main()
-        assert list(
-            open(
-                os.path.join(
-                    os.path.dirname(__file__), "artifacts/cloud99_ocpinventory.json"
-                )
-            )
-        ) == list(
+        cloud99_ocpinventory = await make_env_json("ocpinventory", "cloud99")
+        assert list(cloud99_ocpinventory) == list(
             open(os.path.join(os.path.dirname(__file__), "fixtures/cloud99_env.json"))
         )
