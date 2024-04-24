070aa0f3f6706794751ccde0c8779c8dd78ed741
Gonzalo Rafuls <grafuls@redhat.com>
2024-04-24 17:55:00 +0200

    web: make instack on demand
    Change-Id: Ibf78f9c90af2e26bdb3ab15c58a875c65deca225

--- tests/tools/test_table.py
+++ tests/tools/test_table.py
@@ -4,19 +4,14 @@ from datetime import datetime
 
 from quads.config import Config
 from quads.tools.simple_table_web import main as web_main
-from tests.tools.test_base import TestBase
 
 
-class TestTable(TestBase):
+class TestTable:
     def test_simple_table_web(self):
         Config.__setattr__("foreman_unavailable", True)
-        Config.__setattr__(
-            "visual_web_dir", os.path.join(os.path.dirname(__file__), "artifacts/")
-        )
+        Config.__setattr__("visual_web_dir", os.path.join(os.path.dirname(__file__), "artifacts/"))
         web_main()
-        current = open(
-            os.path.join(os.path.dirname(__file__), "artifacts/current.html"), "r"
-        )
+        current = open(os.path.join(os.path.dirname(__file__), "artifacts/current.html"), "r")
         current = [n for n in current.readlines() if "Emoji" not in n]
         current = "".join(current)
         response = f"""title=
