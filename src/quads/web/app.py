bbc121cd6e44c60c0d5d1952346335e425e6c541
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-05-14 13:20:14 +0530

    wiki page: added the inventory page
    Change-Id: I9bb22ff793d3b38091bc74cc5ad74d3f67f52e44

--- src/quads/web/app.py
+++ src/quads/web/app.py
@@ -1,18 +1,26 @@
-import json
+import asyncio
+import os
+import pathlib
+import re
 from datetime import datetime, time
 
-from flask import render_template, request, jsonify
 from flask import Flask
+from flask import render_template, request, jsonify
 
-from quads.web.forms import ModelSearchForm
 from quads.config import Config
 from quads.quads_api import QuadsApi as Quads, APIServerException, APIBadRequest
+from quads.tools.external.foreman import Foreman
+from quads.web.forms import ModelSearchForm
 
 flask_app = Flask(__name__)
 flask_app.url_map.strict_slashes = False
 flask_app.secret_key = "flask rocks!"
 
 quads = Quads(Config)
+loop = asyncio.new_event_loop()
+foreman = Foreman(Config["foreman_api_url"],
+                  Config["foreman_username"],
+                  Config["foreman_password"], loop=loop)
 
 
 @flask_app.route("/", methods=["GET", "POST"])
@@ -65,6 +73,81 @@ def available(search):
         return jsonify({})
 
     return jsonify(available_hosts)
+
+
+def consolidate_ipmi_data(_host, _path, _value):
+    ipmi_path = os.path.join(Config["data_dir"], "ipmi")
+    host_path = os.path.join(ipmi_path, _host)
+    _file_path = os.path.join(host_path, _path)
+    try:
+        with open(_file_path, "r+") as _ipmi_file:
+            mac = _ipmi_file.read()
+            if not mac and _value:
+                _ipmi_file.seek(0)
+                _ipmi_file.write(_value)
+                _ipmi_file.truncate()
+    except IOError:
+        if not os.path.exists(host_path):
+            pathlib.Path(host_path).mkdir(parents=True, exist_ok=True)
+        with open(_file_path, "w") as _ipmi_file:
+            value = _value if _value else ""
+            _ipmi_file.write(value)
+
+
+@flask_app.route("/wiki")
+def scale_lab_wiki():
+    all_hosts = loop.run_until_complete(foreman.get_all_hosts())
+    blacklist = re.compile("|".join([re.escape(word) for word in Config["exclude_hosts"].split("|")]))
+    hosts = {}
+    for host, properties in all_hosts.items():
+        if not blacklist.search(host):
+            if properties.get("sp_name", False):
+                properties["host_ip"] = properties["ip"]
+                properties["host_mac"] = properties["mac"]
+                properties["ip"] = properties.get("sp_ip")
+                properties["mac"] = properties.get("sp_mac")
+                consolidate_ipmi_data(host, "macaddr", properties["host_mac"])
+                consolidate_ipmi_data(host, "oobmacaddr", properties.get("sp_mac"))
+                svctag_file = os.path.join(Config["data_dir"], "ipmi", host, "svctag")
+                svctag = ""
+                if os.path.exists(svctag_file):
+                    with open(svctag_file) as _file:
+                        svctag = _file.read()
+                properties["svctag"] = svctag.strip()
+                hosts[host] = properties
+    all_hosts = {}
+    headers = [
+        "U",
+        "ServerHostnamePublic",
+        "Serial",
+        "MAC",
+        "IP",
+        "IPMIADDR",
+        "IPMIURL",
+        "IPMIMAC",
+        "Workload",
+        "Owner",
+    ]
+    for rack in Config["racks"].split():
+        for host, properties in hosts.items():
+            if rack in host:
+                host_obj = quads.get_host(host)
+                if host_obj and not host_obj.retired:
+                    assignment = quads.get_active_cloud_assignment(host_obj.cloud.name)
+                    owner = assignment.owner if assignment else "QUADS"
+                    all_hosts.setdefault(rack, []).append({
+                        "U": host_obj.name.split("-")[1][1:],
+                        "ServerHostnamePublic": host_obj.name.split(".")[0],
+                        "Serial": properties.get("svctag", ""),
+                        "MAC": properties.get("host_mac", ""),
+                        "IP": properties.get("host_ip", ""),
+                        "IPMIADDR": properties.get("ip", ""),
+                        "IPMIURL": host_obj.name,
+                        "IPMIMAC": properties.get("mac", ""),
+                        "Workload": host_obj.cloud.name,
+                        "Owner": owner,
+                    })
+    return render_template("wiki.html", headers=headers, all_hosts=all_hosts)
 
 
 if __name__ == "__main__":
