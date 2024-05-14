1834cb94035bc36754f72379f086a6485c1fa184
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-05-14 13:20:14 +0530

    wiki page: added the inventory page
    Change-Id: I9bb22ff793d3b38091bc74cc5ad74d3f67f52e44
    
    Added the assignments page
    
    Change-Id: I288be9986d9c2309e2a13de6457f17f2d0f16d1c

--- src/quads/web/app.py
+++ src/quads/web/app.py
@@ -1,18 +1,26 @@
-import json
+import asyncio
+import os
+import re
 from datetime import datetime, time
 
-from flask import render_template, request, jsonify
 from flask import Flask
+from flask import render_template, request, jsonify
 
-from quads.web.forms import ModelSearchForm
 from quads.config import Config
 from quads.quads_api import QuadsApi as Quads, APIServerException, APIBadRequest
+from quads.tools.external.foreman import Foreman
+from quads.web.controller.CloudOperations import CloudOperations
+from quads.web.forms import ModelSearchForm
 
 flask_app = Flask(__name__)
 flask_app.url_map.strict_slashes = False
 flask_app.secret_key = "flask rocks!"
 
 quads = Quads(Config)
+loop = asyncio.new_event_loop()
+foreman = Foreman(Config["foreman_api_url"],
+                  Config["foreman_username"],
+                  Config["foreman_password"], loop=loop)
 
 
 @flask_app.route("/", methods=["GET", "POST"])
@@ -67,6 +75,79 @@ def available(search):
     return jsonify(available_hosts)
 
 
+@flask_app.route("/wiki/rdu2-scale-lab-dashboard")
+def create_inventory():
+    all_hosts = loop.run_until_complete(foreman.get_all_hosts())
+    blacklist = re.compile("|".join([re.escape(word) for word in Config["exclude_hosts"].split("|")]))
+    hosts = {}
+    for host, properties in all_hosts.items():
+        if not blacklist.search(host):
+            if properties.get("sp_name", False):
+                properties["host_ip"] = properties["ip"]
+                properties["host_mac"] = properties["mac"]
+                properties["ip"] = properties.get("sp_ip")
+                properties["mac"] = properties.get("sp_mac")
+                svctag_file = os.path.join(Config["data_dir"], "ipmi", host, "svctag")
+                svctag = ""
+                if os.path.exists(svctag_file):
+                    with open(svctag_file) as _file:
+                        svctag = _file.read()
+                properties["svctag"] = svctag.strip()
+                hosts[host] = properties
+    all_hosts = {}
+    headers = [
+        "U",
+        "ServerHostnamePublic",
+        "Serial",
+        "MAC",
+        "IP",
+        "IPMIADDR",
+        "IPMIURL",
+        "IPMIMAC",
+        "Workload",
+        "Owner",
+    ]
+    for rack in Config["racks"].split():
+        for host, properties in hosts.items():
+            if rack in host:
+                host_obj = quads.get_host(host)
+                if host_obj and not host_obj.retired:
+                    assignment = quads.get_active_cloud_assignment(host_obj.cloud.name)
+                    owner = assignment.owner if assignment else "QUADS"
+                    all_hosts.setdefault(rack, []).append({
+                        "U": host_obj.name.split("-")[1][1:],
+                        "ServerHostnamePublic": host_obj.name.split(".")[0],
+                        "Serial": properties.get("svctag", ""),
+                        "MAC": properties.get("host_mac", ""),
+                        "IP": properties.get("host_ip", ""),
+                        "IPMIADDR": properties.get("ip", ""),
+                        "IPMIURL": host_obj.name,
+                        "IPMIMAC": properties.get("mac", ""),
+                        "Workload": host_obj.cloud.name,
+                        "Owner": owner,
+                    })
+    return render_template("wiki/inventory.html", headers=headers, all_hosts=all_hosts)
+
+
+@flask_app.route("/wiki")
+def create_assignments():
+
+    headers = ["NAME", "SUMMARY", "OWNER", "REQUEST", "STATUS", "OSPENV", "OCPINV"]
+    host_headers = ["ServerHostnamePublic", "OutOfBand", "DateStartAssignment", "DateEndAssignment", "TotalDuration",
+                    "TimeRemaining"]
+    cloud_operation = CloudOperations(quads_api=quads, foreman=foreman, loop=loop)
+    clouds_summary = cloud_operation.get_cloud_summary_report()
+    daily_utilization = cloud_operation.get_daily_utilization()
+    managed_nodes = cloud_operation.get_managed_nodes()
+    domain_broken_hosts = cloud_operation.get_domain_broken_hosts(domain=Config["domain"])
+    unmanaged_hosts = cloud_operation.get_unmanaged_hosts(exclude_hosts=Config["exclude_hosts"])
+    return render_template("wiki/assignments.html", headers=headers, clouds_summary=clouds_summary,
+                           ticket_url=Config.get('ticket_url'), ticket_queue=Config.get('ticket_queue'),
+                           quads_url=Config.get('quads_url'), openshift_management=Config["openshift_management"],
+                           daily_utilization=daily_utilization, domain_broken_hosts=domain_broken_hosts,
+                           host_headers=host_headers, managed_nodes=managed_nodes, unmanaged_hosts=unmanaged_hosts)
+
+
 if __name__ == "__main__":
-    flask_app.debug = False
+    flask_app.debug = True
     flask_app.run(host="0.0.0.0", port=5001)
