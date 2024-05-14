7aaa183f4dd2a805e41c9f7291ad8c0d1d58cd9c
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-05-14 13:20:14 +0530

    feat: Removed wiki in lieu of quads-web
    Removed wordpress dependency
    Closes: https://github.com/redhat-performance/quads/issues/475
    
    Change-Id: I288be9986d9c2309e2a13de6457f17f2d0f16d1c

--- tests/cli/test_regen.py
+++ tests/cli/test_regen.py
@@ -1,11 +1,9 @@
 import os
 
 from datetime import datetime
-from unittest.mock import patch
 
 from quads.config import Config
 from tests.cli.test_base import TestBase
-from tests.tools.test_regenerate_wiki import WikiStub
 
 
 class TestRegen(TestBase):
@@ -38,18 +36,3 @@ class TestRegen(TestBase):
             "Regenerated 'instackenv' for OpenStack Management.",
             "Regenerated 'ocpinventory' for OpenShift Management.",
         ]
-
-    @patch("quads.tools.regenerate_wiki.Wordpress", WikiStub)
-    @patch("quads.tools.regenerate_vlans_wiki.Wordpress", WikiStub)
-    def test_regen_wiki(self):
-        Config.__setattr__("foreman_unavailable", True)
-        Config.__setattr__(
-            "wp_wiki_git_repo_path",
-            os.path.join(os.path.dirname(__file__), "artifacts/git/wiki"),
-        )
-        self.quads_cli_call("regen_wiki")
-
-        files = ["assignments.md", "main.md"]
-        for f in files:
-            assert os.path.exists(os.path.join(os.path.dirname(__file__), f"artifacts/git/wiki/{f}"))
-        assert "Regenerated wiki." in self._caplog.messages
