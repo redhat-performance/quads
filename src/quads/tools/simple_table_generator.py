6e3f31ccc0e07996f5f854842284a05a181f297e
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-08-27 12:52:43 +0530

    Async func to get_current_schedules
    Change-Id: I8a8d1f9beec752c4bb84894218afca19f9170407

--- src/quads/tools/simple_table_generator.py
+++ src/quads/tools/simple_table_generator.py
@@ -1,14 +1,56 @@
 #!/usr/bin/env python3
-
 import argparse
-import os
+import asyncio
 import csv
-from datetime import datetime
+import os
 import random
+from datetime import datetime
+from typing import List
+from urllib import parse as url_parse
 
+import aiohttp
+from aiohttp import BasicAuth
 from jinja2 import Template
+from requests import Response
+
 from quads.config import Config
 from quads.quads_api import QuadsApi
+from quads.server.models import Schedule
+
+
+class QuadsApiAsync:
+
+    def __init__(self, config: Config):
+        self.config = config
+        self.base_url = config.API_URL
+
+    async def async_get(self, endpoint: str) -> Response:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                        os.path.join(self.base_url, endpoint),
+                        auth=BasicAuth(self.config.get("quads_api_username"), self.config.get("quads_api_password")),
+                        ssl=False,
+                        timeout=60,
+                ) as response:
+                    result = await response.json()
+        except Exception as ex:
+            result = {}
+        return result
+
+    async def async_get_current_schedules(self, data: dict = None) -> List[Schedule]:
+        if data is None:
+            data = {}
+        endpoint = os.path.join("schedules", "current")
+        url = f"{endpoint}"
+        if data:
+            url_params = url_parse.urlencode(data)
+            url = f"{endpoint}?{url_params}"
+        response = await self.async_get(url)
+        schedules = []
+        for schedule in response:
+            schedules.append(Schedule().from_dict(schedule))
+        return schedules
 
 
 def random_color():
@@ -18,8 +60,69 @@ def random_color():
     return "#%02X%02X%02X" % (rand(), rand(), rand())
 
 
-def generator(_host_file, _days, _month, _year, _gentime):
-    quads = QuadsApi(Config)
+quads = QuadsApi(Config)
+quads_async = QuadsApiAsync(Config)
+all_samples = []
+all_samples.extend(range(129296, 129510))
+all_samples.extend(range(128000, 128252))
+samples = random.sample(all_samples, 200)
+exclude = [129401, 129484]
+emojis = [emoji for emoji in samples if emoji not in exclude]
+colors = [random_color() for _ in range(100)]
+colors[0] = "#A9A9A9"
+
+
+async def fetch_schedule(payload):
+    schedules = await quads_async.async_get_current_schedules(payload)
+    if schedules:
+        schedule = schedules[0]
+        chosen_color = schedule.assignment.cloud.name[5:]
+        return schedule, chosen_color
+    else:
+        return None, "01"
+
+
+async def process_hosts(host, _days, _month, _year):
+    non_allocated_count = 0
+    __days = []
+    tasks = []
+
+    for j in range(1, _days + 1):
+        cell_date = "%s-%.2d-%.2d 01:00" % (_year, _month, j)
+        cell_time = datetime.strptime(cell_date, "%Y-%m-%d %H:%M")
+        datearg_iso = cell_time.isoformat()
+        date_str = ":".join(datearg_iso.split(":")[:-1])
+        payload = {"host": host.name, "date": date_str}
+        tasks.append(fetch_schedule(payload))
+
+    results = await asyncio.gather(*tasks)
+
+    for j, (schedule, chosen_color) in enumerate(results, 1):
+        cell_date = "%s-%.2d-%.2d 01:00" % (_year, _month, j)
+        cell_time = datetime.strptime(cell_date, "%Y-%m-%d %H:%M")
+        _day = {
+            "day": j,
+            "chosen_color": chosen_color,
+            "emoji": "&#%s;" % emojis[int(chosen_color) - 1],
+            "color": colors[int(chosen_color) - 1],
+            "cell_date": cell_date,
+            "cell_time": cell_time,
+        }
+
+        if schedule:
+            assignment = schedule.assignment
+            _day["display_description"] = assignment.description
+            _day["display_owner"] = assignment.owner
+            _day["display_ticket"] = assignment.ticket
+        else:
+            non_allocated_count += 1
+
+        __days.append(_day)
+
+    return __days, non_allocated_count
+
+
+async def generator(_host_file, _days, _month, _year, _gentime):
     if _host_file:
         with open(_host_file, "r") as f:
             reader = csv.reader(f)
@@ -31,49 +134,12 @@ def generator(_host_file, _days, _month, _year, _gentime):
     lines = []
     __days = []
     non_allocated_count = 0
-    all_samples = []
-    all_samples.extend(range(129296, 129510))
-    all_samples.extend(range(128000, 128252))
-    samples = random.sample(all_samples, 200)
-    exclude = [129401, 129484]
-    emojis = [emoji for emoji in samples if emoji not in exclude]
-    colors = [random_color() for _ in range(100)]
-    colors[0] = "#A9A9A9"
-    for i, host in enumerate(hosts):
-        line = {"hostname": host.name}
-        __days = []
-        for j in range(1, _days + 1):
-            cell_date = "%s-%.2d-%.2d 01:00" % (_year, _month, j)
-            cell_time = datetime.strptime(cell_date, "%Y-%m-%d %H:%M")
-            datearg_iso = cell_time.isoformat()
-            date_str = ":".join(datearg_iso.split(":")[:-1])
-            payload = {"host": host.name, "date": date_str}
-            schedule = None
-            schedules = quads.get_current_schedules(payload)
-            if schedules:
-                schedule = schedules[0]
-                chosen_color = schedule.assignment.cloud.name[5:]
-            else:
-                non_allocated_count += 1
-                chosen_color = "01"
-            _day = {
-                "day": j,
-                "chosen_color": chosen_color,
-                "emoji": "&#%s;" % emojis[int(chosen_color) - 1],
-                "color": colors[int(chosen_color) - 1],
-                "cell_date": cell_date,
-                "cell_time": cell_time,
-            }
+    tasks = [process_hosts(host, _days, _month, _year) for host in hosts]
+    results = await asyncio.gather(*tasks)
 
-            if schedule:
-                assignment = schedule.assignment
-                _day["display_description"] = assignment.description
-                _day["display_owner"] = assignment.owner
-                _day["display_ticket"] = assignment.ticket
-            __days.append(_day)
-
-        line["days"] = __days
-        lines.append(line)
+    for host, (days, non_allocated) in zip(hosts, results):
+        lines.append({"hostname": host.name, "days": days})
+        non_allocated_count += non_allocated
 
     total_hosts = len(hosts)
     total_use = len(quads.get_current_schedules())
@@ -94,7 +160,7 @@ def generator(_host_file, _days, _month, _year, _gentime):
     return content
 
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Generate a simple HTML table with color depicting resource usage for the month"
     )
